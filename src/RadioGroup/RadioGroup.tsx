2e7e9389c038a14f4b8bddc19246906b7bc5bf13
Al-Raheem <XYMF@corp1.ad1.seb.net>
2019-05-09 14:35:41 +0800

    fix: fixes #11
    fixes issue #11 causing radio button conflicts in radiogroup component
    
    #11

--- src/RadioGroup/RadioGroup.tsx
+++ src/RadioGroup/RadioGroup.tsx
@@ -31,25 +31,26 @@ export const RadioGroup: React.FunctionComponent<RadioGroupProps> = (props: Radi
             <div className={inputFieldClass}>
                 {props.label && <label className="radio-group-label" htmlFor={props.name}>{props.label}</label>}
 
-                {props.list && props.list.map((item, index) =>
-                    <div key={index} className="radio-item">
-
-                        <label className="radio-label" htmlFor={item.label}>{item.label}</label>
-                        <input
-                            className="radio-input"
-                            type="radio"
-                            value={item.value}
-                            name={item.group}
-                            id={item.label}
-                            checked={props.value === item.value}
-                            disabled={props.disableAll || item.disabled}
-                            onChange={(e) => { props.onChange(item.value); }}
-                        />
-
-                        <span className="checkmark" />
-                        {item.description && <span className="radio-description">{item.description}</span>}
-                    </div>
-                )}
+                {props.list && props.list.map((item, index) => {
+                    const identifier: string = item.label.replace(" ", "_") + Math.floor(Math.random() * 100) + (new Date()).getTime();
+                    return (
+                        <div key={index} className="radio-item">
+                            <label className="radio-label" htmlFor={identifier}>{item.label}</label>
+                            <input
+                                className="radio-input"
+                                type="radio"
+                                value={item.value}
+                                name={item.group}
+                                id={identifier}
+                                checked={props.value === item.value}
+                                disabled={props.disableAll || item.disabled}
+                                onChange={(e) => { props.onChange(item.value); }}
+                            />
+                            <span className="checkmark" />
+                            {item.description && <span className="radio-description">{item.description}</span>}
+                        </div>
+                    );
+                })}
                 {props.error && <div className={"alert alert-danger"}>{props.error}</div>}
             </div>
         </div>
