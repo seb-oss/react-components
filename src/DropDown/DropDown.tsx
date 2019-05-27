c92d32441926759a59cff17e5ef5e40541d00ece
Subotic <S7386B@corp1.ad1.seb.net>
2019-05-27 18:04:29 +0800

    test(dropdown-component): added test cases and refactoring
    added some missing cases in tests and refactored some of the code
    
    re #19

--- src/DropDown/DropDown.tsx
+++ src/DropDown/DropDown.tsx
@@ -22,8 +22,8 @@ export interface DropDownProps {
 }
 
 const chevronDownIcon: JSX.Element = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M443.5 162.6l-7.1-7.1c-4.7-4.7-12.3-4.7-17 0L224 351 28.5 155.5c-4.7-4.7-12.3-4.7-17 0l-7.1 7.1c-4.7 4.7-4.7 12.3 0 17l211 211.1c4.7 4.7 12.3 4.7 17 0l211-211.1c4.8-4.7 4.8-12.3.1-17z" /></svg>;
-const timesIcon: JSX.Element = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M217.5 256l137.2-137.2c4.7-4.7 4.7-12.3 0-17l-8.5-8.5c-4.7-4.7-12.3-4.7-17 0L192 230.5 54.8 93.4c-4.7-4.7-12.3-4.7-17 0l-8.5 8.5c-4.7 4.7-4.7 12.3 0 17L166.5 256 29.4 393.2c-4.7 4.7-4.7 12.3 0 17l8.5 8.5c4.7 4.7 12.3 4.7 17 0L192 281.5l137.2 137.2c4.7 4.7 12.3 4.7 17 0l8.5-8.5c4.7-4.7 4.7-12.3 0-17L217.5 256z" /></svg>;
-const moreIcon: JSX.Element = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M192 256c0 17.7-14.3 32-32 32s-32-14.3-32-32 14.3-32 32-32 32 14.3 32 32zm88-32c-17.7 0-32 14.3-32 32s14.3 32 32 32 32-14.3 32-32-14.3-32-32-32zm-240 0c-17.7 0-32 14.3-32 32s14.3 32 32 32 32-14.3 32-32-14.3-32-32-32z"/></svg>;
+const timesIcon: JSX.Element = <svg id="dropdown-times-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M217.5 256l137.2-137.2c4.7-4.7 4.7-12.3 0-17l-8.5-8.5c-4.7-4.7-12.3-4.7-17 0L192 230.5 54.8 93.4c-4.7-4.7-12.3-4.7-17 0l-8.5 8.5c-4.7 4.7-4.7 12.3 0 17L166.5 256 29.4 393.2c-4.7 4.7-4.7 12.3 0 17l8.5 8.5c4.7 4.7 12.3 4.7 17 0L192 281.5l137.2 137.2c4.7 4.7 12.3 4.7 17 0l8.5-8.5c4.7-4.7 4.7-12.3 0-17L217.5 256z" /></svg>;
+const moreIcon: JSX.Element = <svg id="dropdown-more-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M192 256c0 17.7-14.3 32-32 32s-32-14.3-32-32 14.3-32 32-32 32 14.3 32 32zm88-32c-17.7 0-32 14.3-32 32s14.3 32 32 32 32-14.3 32-32-14.3-32-32-32zm-240 0c-17.7 0-32 14.3-32 32s14.3 32 32 32 32-14.3 32-32-14.3-32-32-32z" /></svg>;
 // helper function for updating object in array
 function updateObjectInArray<T extends object>(array: Array<T>, index: number, item: T): Array<T> {
     return array.map((v, k) => {
@@ -44,6 +44,7 @@ export const DropDown: React.FunctionComponent<DropDownProps> = (props: DropDown
     const selectedList = props.list.filter((e) => e.selected);
     const displayList = props.list.filter((e) => e.label.includes(searchText));
     const allSelected = selectedList.length === props.list.length;
+    const shouldDisable = (props.disabled || !props.list.length);
 
     // Adding event listener to listen to clicks outside the component on mount, removing on unmount
     React.useEffect(() => {
@@ -53,6 +54,12 @@ export const DropDown: React.FunctionComponent<DropDownProps> = (props: DropDown
         };
     });
 
+    const handleClickOutside = (event) => {
+        if ((dropdownToggleRef.current && !dropdownToggleRef.current.contains(event.target)) && (dropdownMenuRef.current && !dropdownMenuRef.current.contains(event.target))) {
+            setOpen(false);
+        }
+    };
+
     // As soon as the menu opens focus the search text input
     React.useEffect(() => {
         if (open && searchRef.current) {
@@ -60,14 +67,9 @@ export const DropDown: React.FunctionComponent<DropDownProps> = (props: DropDown
         }
     }, [open]);
 
-    const handleClickOutside = (event) => {
-        if ((dropdownToggleRef.current && !dropdownToggleRef.current.contains(event.target)) && (dropdownMenuRef.current && !dropdownMenuRef.current.contains(event.target))) {
-            setOpen(false);
-        }
-    };
-
-    const handleClickClear = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-        event.stopPropagation();
+    // Function that runs when the clean icon is pressed
+    const handleClickClear = (event: React.MouseEvent<HTMLDivElement, MouseEvent>): void => {
+        event.preventDefault();
         const newList = props.list.map((item) => {
             return { ...item, selected: false };
         });
@@ -75,12 +77,45 @@ export const DropDown: React.FunctionComponent<DropDownProps> = (props: DropDown
         !props.multi && setOpen(false);
     };
 
+    // Function that runs when the select all option is pressed
+    const handleClickSelectAll = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
+        event.stopPropagation();
+        const newList = props.list.map((item) => {
+            return { ...item, selected: (allSelected) ? false : true };
+        });
+        props.onChange(newList, props.name);
+    };
+
+    // Function which will run when the item is clicked
+    const dropdownRowSelected = (item: DropDownItem, index: number): void => {
+        if (!props.multi) {
+            const newItem = { ...item, selected: props.clearable ? !item.selected : true };
+            const cleanList = props.list.map((el) => {
+                return { ...el, selected: false };
+            });
+            const newList = updateObjectInArray(cleanList, index, newItem);
+            props.onChange(newList, props.name);
+            setOpen(false);
+        } else {
+            const newItem = { ...item, selected: !item.selected };
+            const newList = updateObjectInArray(props.list, index, newItem);
+            props.onChange(newList, props.name);
+        }
+    };
+
+    // Returns the appropriate title for different situations and component types
     const getTitleLabel = () => {
+        if (props.list && props.list.length === 0) {
+            return "Empty";
+        }
         if (selectedList.length > 0) {
             if (selectedList.length === props.list.length) {
                 return `All selected (${selectedList.length})`;
             }
             if (props.multi) {
+                if (selectedList.length === 1) {
+                    return selectedList[0].label;
+                }
                 return selectedList.length + " Selected"; // TODO should be like this example: 1st Item, 2nd Item... (+2)
             }
             return selectedList[0].label;
@@ -89,39 +124,45 @@ export const DropDown: React.FunctionComponent<DropDownProps> = (props: DropDown
         return (props.placeholder && props.placeholder.length) ? props.placeholder : "Select ...";
     };
 
-    const toggleClassName = `btn btn-secondary custom-dropdown-toggle${open ? " open" : ""}`;
-
     return (
-        <div className={`dropdown custom-dropdown${props.disabled ? " disabled" : ""}${props.className ? " " + props.className : ""}`}>
+        <div className={`dropdown custom-dropdown${shouldDisable ? " disabled" : ""}${props.className ? " " + props.className : ""}`}>
             {props.label && <label className="dropdown-label">{props.label}</label>}
 
             <button
-                disabled={props.disabled}
+                disabled={shouldDisable}
                 ref={dropdownToggleRef}
-                className={`${toggleClassName}${props.more ? " more mx-right" : ""}`}
+                className={`btn btn-secondary custom-dropdown-toggle${open ? " open" : ""}${props.more ? " more mx-right" : ""}`}
                 type="button"
                 id="dropdownMenuButton"
                 onClick={(e) => setOpen(!open)}
             >
                 {!props.more ?
-                <>
-                    <div className="title">
-                        {getTitleLabel()}
-                    </div>
+                    <>
+                        <div className="title">
+                            {getTitleLabel()}
+                        </div>
 
-                    <div className="right-items">
-                        {((props.clearable || props.multi) && selectedList.length > 0) ? <div className="icon-holder" onClick={handleClickClear}>{timesIcon}</div> : null}
-                        <div className="icon-holder chevron">{chevronDownIcon}</div>
-                    </div>
-                </> :
-                <div className="right-items"><div className="icon-holder">{moreIcon}</div></div>
+                        <div className="right-items">
+                            {((props.clearable || props.multi) && selectedList.length > 0) ? <div className="icon-holder" onClick={handleClickClear}>{timesIcon}</div> : null}
+                            <div className="icon-holder chevron">{chevronDownIcon}</div>
+                        </div>
+                    </> :
+                    <div className="right-items"><div className="icon-holder">{moreIcon}</div></div>
                 }
             </button>
 
             <div ref={dropdownMenuRef} className={`dropdown-menu custom-dropdown-menu${open ? " show" : ""}${props.more ? " dropdown-menu-right" : ""}`} >
                 {props.searchable &&
                     <>
-                        <input ref={searchRef} className="search-input" name="search-input" placeholder="Search ..." value={searchText} onChange={(e) => setSearchText(e.target.value)} />
+                        <input
+                            ref={searchRef}
+                            type="text"
+                            className="search-input"
+                            name="search-input"
+                            placeholder="Search ..."
+                            value={searchText}
+                            onChange={(e) => setSearchText(e.target.value)}
+                        />
                         <div className="dropdown-divider blue" />
                     </>
                 }
@@ -132,13 +173,7 @@ export const DropDown: React.FunctionComponent<DropDownProps> = (props: DropDown
                             key={"select-all-anchor"}
                             className={`dropdown-item custom-dropdown-item multi${(allSelected) ? " selected" : ""}`}
                             href=""
-                            onClick={(e) => {
-                                e.preventDefault();
-                                const newList = props.list.map((item) => {
-                                    return { ...item, selected: (allSelected) ? false : true };
-                                });
-                                props.onChange(newList, props.name);
-                            }}
+                            onClick={handleClickSelectAll}
                         >
                             <div className="custom-control">
                                 <input
@@ -165,19 +200,7 @@ export const DropDown: React.FunctionComponent<DropDownProps> = (props: DropDown
                                 href=""
                                 onClick={(e) => {
                                     e.preventDefault();
-                                    if (!props.multi) {
-                                        const newItem = { ...item, selected: props.clearable ? !item.selected : true };
-                                        const cleanList = props.list.map((el) => {
-                                            return { ...el, selected: false };
-                                        });
-                                        const newList = updateObjectInArray(cleanList, index, newItem);
-                                        props.onChange(newList, props.name);
-                                        setOpen(false);
-                                    } else {
-                                        const newItem = { ...item, selected: !item.selected };
-                                        const newList = updateObjectInArray(props.list, index, newItem);
-                                        props.onChange(newList, props.name);
-                                    }
+                                    dropdownRowSelected(item, index);
                                 }}
                             >
                                 {props.multi ?
--- src/DropDown/Dropdown.test.tsx
+++ src/DropDown/Dropdown.test.tsx
@@ -6,7 +6,7 @@ describe("Component: DropDown", () => {
     const props = {
         onChange: jest.fn(),
         list: [
-            { value: "Male", label: "male", selected: false },
+            { value: "Male", label: "Male", selected: false },
             { value: "Female", label: "Female", selected: false },
         ]
     };
@@ -14,28 +14,160 @@ describe("Component: DropDown", () => {
     it("Should render", () => {
         const wrapper = shallow(<DropDown {...props} />);
         expect(wrapper).toBeDefined();
+        wrapper.unmount();
     });
 
     it("Should pass custom class", () => {
         const wrapper = shallow(<DropDown {...props} className="myDropdown" />);
         expect(wrapper.hasClass("myDropdown")).toBeTruthy();
+        wrapper.unmount();
     });
 
     it("Should render label when passed", () => {
         const wrapper = shallow(<DropDown {...props} label="label" />);
         expect(wrapper.find("label").length).toBe(1);
         expect(wrapper.find("label").text()).toEqual("label");
+        wrapper.unmount();
+    });
+
+    it("Should display \"Empty\" if list prop is an empty array", () => {
+        const wrapper = mount(<DropDown onChange={props.onChange} list={[]} placeholder="myPlaceholder" />);
+        expect(wrapper.find(".title").length).toBe(1);
+        expect(wrapper.find(".title").text()).toEqual("Empty");
+        wrapper.unmount();
     });
 
     it("Should render placeholder", () => {
-        const wrapper2 = mount(<DropDown {...props} placeholder="myPlaceholder" />);
-        expect(wrapper2.find(".title").length).toBe(1);
-        expect(wrapper2.find(".title").text()).toEqual("myPlaceholder");
-        wrapper2.unmount();
+        const wrapper = mount(<DropDown {...props} placeholder="myPlaceholder" />);
+        expect(wrapper.find(".title").length).toBe(1);
+        expect(wrapper.find(".title").text()).toEqual("myPlaceholder");
+        wrapper.unmount();
+    });
+
+    it("Should display item label when one item is selected instead of placeholder", () => {
+        const wrapper = mount(
+            <DropDown
+                onChange={props.onChange}
+                list={[
+                    { value: "Male", label: "Male", selected: true },
+                    { value: "Female", label: "Female", selected: false },
+                ]}
+                placeholder="myPlaceholder"
+            />
+        );
+        expect(wrapper.find(".title").length).toBe(1);
+        expect(wrapper.find(".title").text()).toEqual("Male");
+        wrapper.unmount();
+    });
+
+    it("Should display item label when one item is selected even when multi is enabled", () => {
+        const wrapper = mount(
+            <DropDown
+                onChange={props.onChange}
+                list={[
+                    { value: "Male", label: "Male", selected: true },
+                    { value: "Female", label: "Female", selected: false },
+                ]}
+                placeholder="myPlaceholder"
+                multi={true}
+            />
+        );
+        expect(wrapper.find(".title").length).toBe(1);
+        expect(wrapper.find(".title").text()).toEqual("Male");
+        wrapper.unmount();
+    });
+
+    it("Should display \"[selectedList.length] Selected\" label if selected more than one and less that total amount of items. (multi only)", () => {
+        const wrapper = mount(
+            <DropDown
+                onChange={props.onChange}
+                list={[
+                    { value: "Male", label: "Male", selected: true },
+                    { value: "Female", label: "Female", selected: true },
+                    { value: "Other", label: "Other", selected: false },
+                ]}
+                placeholder="myPlaceholder"
+                multi={true}
+            />
+        );
+        expect(wrapper.find(".title").length).toBe(1);
+        expect(wrapper.find(".title").text()).toEqual("2 Selected");
+        wrapper.unmount();
+    });
+
+    it("Should display label: All selected (list.length) when all items are selected", () => {
+        const wrapper = mount(
+            <DropDown
+                onChange={props.onChange}
+                list={[
+                    { value: "Male", label: "Male", selected: true },
+                    { value: "Female", label: "Female", selected: true },
+                ]}
+                placeholder="myPlaceholder"
+                clearable={true}
+            />
+        );
+        expect(wrapper.find(".title").length).toBe(1);
+        expect(wrapper.find(".title").text()).toEqual("All selected (2)");
+        wrapper.unmount();
     });
 
     it("Should enable searchable prop when set to true", () => {
         const wrapper = mount(<DropDown {...props} searchable={true} />);
         expect(wrapper.find(".search-input").length).toBe(1);
+        wrapper.unmount();
+    });
+
+    it("Should enable more button when set to true", () => {
+        const wrapper = mount(<DropDown {...props} more={true} />);
+        expect(wrapper.find("#dropdown-more-icon").length).toBe(1);
+        expect(wrapper.find(".more").length).toBe(1);
+    });
+
+    it("Should disable component when disabled set to true", () => {
+        const wrapper = mount(<DropDown {...props} disabled={true} />);
+        expect(wrapper.find(".disabled").length).toBe(1);
+        wrapper.unmount();
+    });
+
+    it("Should display clear button when multi prop set to true and at least one item selected", () => {
+        const wrapper = mount(
+            <DropDown
+                onChange={props.onChange}
+                list={[
+                    { value: "Male", label: "Male", selected: true },
+                    { value: "Female", label: "Female", selected: false },
+                ]}
+                multi={true}
+            />
+        );
+        expect(wrapper.find("#dropdown-times-icon").length).toBe(1);
+        wrapper.unmount();
+    });
+
+    it("Should display clear button when clearable prop set to true and at least one item selected", () => {
+        const wrapper = mount(
+            <DropDown
+                onChange={props.onChange}
+                list={[
+                    { value: "Male", label: "Male", selected: true },
+                    { value: "Female", label: "Female", selected: false },
+                ]}
+                clearable={true}
+            />
+        );
+        expect(wrapper.find("#dropdown-times-icon").length).toBe(1);
+        wrapper.unmount();
+    });
+
+    it("Should not display clear button if all items are unselected even when clearable prop set to true", () => {
+        const wrapper = mount(
+            <DropDown
+                {...props}
+                clearable={true}
+            />
+        );
+        expect(wrapper.find("#dropdown-times-icon").length).toBe(0);
+        wrapper.unmount();
     });
 });
