98a68ecdb7c246b00f2885cccc337fb19025c400
Yousif Al-Raheem <yousifalraheem@gmail.com>
2019-09-22 23:06:14 +0800

    refactor(app): cleaning up the app and sidebar

--- develop/components/App.tsx
+++ develop/components/App.tsx
@@ -1,123 +1,88 @@
 import * as React from "react";
 import { Switch, withRouter } from "react-router-dom";
+import { RouteComponentProps, Route, Redirect } from "react-router";
+// Components
 import TitleBar from "./common/TitleBar";
 import SideBar from "./common/SideBar";
 import { Loader } from "../../src/Loader/Loader";
-import { RouteComponentProps, Route } from "react-router";
 import { getParameterByName } from "../utils/queryString";
 
+type RouteItem = { path: string, component: React.LazyExoticComponent<any> };
+
 // Here we are asynchronously loading components based on their path
-const About = React.lazy(() => import("./common/About"));
-const ButtonPage = React.lazy(() => import("./pages/ButtonPage"));
-const TextBoxPage = React.lazy(() => import("./pages/TextBoxPage"));
-const TextBoxGroupPage = React.lazy(() => import("./pages/TextBoxGroupPage"));
-const TextAreaPage = React.lazy(() => import("./pages/TextAreaPage"));
-const TextLabelPage = React.lazy(() => import("./pages/TextLabelPage"));
-const CheckBoxPage = React.lazy(() => import("./pages/CheckBoxPage"));
-const DropdownPage = React.lazy(() => import("./pages/DropdownPage"));
-const DatepickerPage = React.lazy(() => import("./pages/DatepickerPage"));
-const RadioGroupPage = React.lazy(() => import("./pages/RadioGroupPage"));
-const RadioButtonPage = React.lazy(() => import("./pages/RadioButtonPage"));
-const DialoguePage = React.lazy(() => import("./pages/DialoguePage"));
-const TooltipPage = React.lazy(() => import("./pages/TooltipPage"));
-const LoaderPage = React.lazy(() => import("./pages/LoaderPage"));
-const RatingPage = React.lazy(() => import("./pages/RatingPage"));
-const TogglePage = React.lazy(() => import("./pages/TogglePage"));
-const SliderPage = React.lazy(() => import("./pages/SliderPage"));
-const ProgressBarPage = React.lazy(() => import("./pages/ProgressBarPage"));
-const IconPage = React.lazy(() => import("./pages/IconPage"));
-const CarouselPage = React.lazy(() => import("./pages/CarouselPage"));
-const PaginationPage = React.lazy(() => import("./pages/PaginationPage"));
-const ChartPage = React.lazy(() => import("./pages/ChartPage"));
-const StepperPage = React.lazy(() => import("./pages/StepperPage"));
-const ImagePage = React.lazy(() => import("./pages/ImagePage"));
-const TimelinePage = React.lazy(() => import("./pages/TimelinePage"));
-const StepTrackerPage = React.lazy(() => import("./pages/StepTrackerPage"));
-const BreadcrumbPage = React.lazy(() => import("./pages/BreadcrumbPage"));
-const AccordionPage = React.lazy(() => import("./pages/AccordionPage"));
-const InlineLinkPage = React.lazy(() => import("./pages/InlineLinkPage"));
-const TabsPage = React.lazy(() => import("./pages/TabsPage"));
-const VideoPage = React.lazy(() => import("./pages/VideoPage"));
-const NotificationPage = React.lazy(() => import("./pages/NotificationPage"));
-const TimepickerPage = React.lazy(() => import("./pages/TimepickerPage"));
-const ImageCropperPage = React.lazy(() => import("./pages/ImageCropperPage"));
-const TimerPage = React.lazy(() => import("./pages/TimerPage"));
-const ModalPage = React.lazy(() => import("./pages/ModalPage"));
-const NotFound = React.lazy(() => import("./common/NotFound"));
+const routes: Array<RouteItem> = [
+    { path: "/about", component: React.lazy(() => import("./common/About")) },
+    // --- Components -----
+    { path: "/accordion", component: React.lazy(() => import("./pages/AccordionPage")) },
+    { path: "/breadcrumb", component: React.lazy(() => import("./pages/BreadcrumbPage")) },
+    { path: "/button", component: React.lazy(() => import("./pages/ButtonPage")) },
+    { path: "/carousel", component: React.lazy(() => import("./pages/CarouselPage")) },
+    { path: "/chart", component: React.lazy(() => import("./pages/ChartPage")) },
+    { path: "/checkbox", component: React.lazy(() => import("./pages/CheckBoxPage")) },
+    { path: "/datepicker", component: React.lazy(() => import("./pages/DatepickerPage")) },
+    { path: "/dialogue", component: React.lazy(() => import("./pages/DialoguePage")) },
+    { path: "/dropdown", component: React.lazy(() => import("./pages/DropdownPage")) },
+    { path: "/icons", component: React.lazy(() => import("./pages/IconPage")) },
+    { path: "/image", component: React.lazy(() => import("./pages/ImageCropperPage")) },
+    { path: "/imagecropper", component: React.lazy(() => import("./pages/ImagePage")) },
+    { path: "/inlinelink", component: React.lazy(() => import("./pages/InlineLinkPage")) },
+    { path: "/loader", component: React.lazy(() => import("./pages/LoaderPage")) },
+    { path: "/modal", component: React.lazy(() => import("./pages/ModalPage")) },
+    { path: "/notification", component: React.lazy(() => import("./pages/NotificationPage")) },
+    { path: "/pagination", component: React.lazy(() => import("./pages/PaginationPage")) },
+    { path: "/progressbar", component: React.lazy(() => import("./pages/ProgressBarPage")) },
+    { path: "/radiobtn", component: React.lazy(() => import("./pages/RadioButtonPage")) },
+    { path: "/radiogroup", component: React.lazy(() => import("./pages/RadioGroupPage")) },
+    { path: "/rating", component: React.lazy(() => import("./pages/RatingPage")) },
+    { path: "/slider", component: React.lazy(() => import("./pages/SliderPage")) },
+    { path: "/stepper", component: React.lazy(() => import("./pages/StepperPage")) },
+    { path: "/steptracker", component: React.lazy(() => import("./pages/StepTrackerPage")) },
+    { path: "/tabs", component: React.lazy(() => import("./pages/TabsPage")) },
+    { path: "/textarea", component: React.lazy(() => import("./pages/TextAreaPage")) },
+    { path: "/textbox", component: React.lazy(() => import("./pages/TextBoxGroupPage")) },
+    { path: "/textboxgroup", component: React.lazy(() => import("./pages/TextBoxPage")) },
+    { path: "/textlabel", component: React.lazy(() => import("./pages/TextLabelPage")) },
+    { path: "/timeline", component: React.lazy(() => import("./pages/TimelinePage")) },
+    { path: "/timepicker", component: React.lazy(() => import("./pages/TimepickerPage")) },
+    { path: "/timer", component: React.lazy(() => import("./pages/TimerPage")) },
+    { path: "/toggle", component: React.lazy(() => import("./pages/TogglePage")) },
+    { path: "/tooltip", component: React.lazy(() => import("./pages/TooltipPage")) },
+    { path: "/video", component: React.lazy(() => import("./pages/VideoPage")) },
+    // --- End Components -----
+    { path: "*", component: React.lazy(() => import("./common/NotFound")) }
+];
 
-interface AppState {
-    sidebarToggle: boolean;
-}
+const storedSidebarToggle: boolean = localStorage.getItem("sidebar") === null ? true : JSON.parse(localStorage.getItem("sidebar"));
 
-class App extends React.Component<RouteComponentProps, AppState> {
-    constructor(props: any) {
-        super(props);
+const App: React.FunctionComponent<RouteComponentProps> = (props: RouteComponentProps) => {
+    const [sidebarToggle, setSidebarToggle] = React.useState<boolean>(storedSidebarToggle);
+    const [brief, setBrief] = React.useState<boolean>(false);
 
-        const sidebarToggle: boolean = localStorage.getItem("sidebar") === null
-            ? undefined
-            : JSON.parse(localStorage.getItem("sidebar"));
+    React.useEffect(() => {
+        const mode: string = getParameterByName(props.location.search, "mode");
+        const isBrief: boolean = mode && mode.toLowerCase() === "dl";
+        (isBrief !== brief) && setBrief(isBrief);
+    }, [props.location]);
 
-        this.state = {
-            sidebarToggle: sidebarToggle !== undefined ? sidebarToggle : true
-        };
+    function toggleSidebar(): void {
+        localStorage.setItem("sidebar", String(!sidebarToggle));
+        setSidebarToggle(!sidebarToggle);
     }
 
-    toggleSidebar = () => this.setState({ sidebarToggle: !this.state.sidebarToggle }, () => localStorage.setItem("sidebar", String(this.state.sidebarToggle)));
-
-    render() {
-        const mode: string = getParameterByName(this.props.location.search, "mode");
-        const brief: string = mode && mode.toLowerCase() === "dl" ? " brief" : "";
-        return (
-            <div className="app-container">
-                <TitleBar onToggleClick={this.toggleSidebar} history={this.props.history} />
-                <div className={"route-holder" + (this.state.sidebarToggle ? " sidebar-opened" : "") + brief}>
-                    <React.Suspense fallback={<Loader toggle={true} fullscreen={true} />}>
-                        <Switch>
-                            <Route path="/" exact={true} component={About} />
-                            <Route path={"/about"} component={About} />
-                            <Route path={"/button"} component={ButtonPage} />
-                            <Route path={"/textbox"} component={TextBoxPage} />
-                            <Route path={"/textboxgroup"} component={TextBoxGroupPage} />
-                            <Route path={"/textarea"} component={TextAreaPage} />
-                            <Route path={"/textlabel"} component={TextLabelPage} />
-                            <Route path={"/checkbox"} component={CheckBoxPage} />
-                            <Route path={"/dropdown"} component={DropdownPage} />
-                            <Route path={"/datepicker"} component={DatepickerPage} />
-                            <Route path={"/radiogroup"} component={RadioGroupPage} />
-                            <Route path={"/radiobtn"} component={RadioButtonPage} />
-                            <Route path={"/dialogue"} component={DialoguePage} />
-                            <Route path={"/modal"} component={ModalPage} />
-                            <Route path={"/tooltip"} component={TooltipPage} />
-                            <Route path={"/loader"} component={LoaderPage} />
-                            <Route path={"/rating"} component={RatingPage} />
-                            <Route path={"/toggle"} component={TogglePage} />
-                            <Route path={"/slider"} component={SliderPage} />
-                            <Route path={"/progressbar"} component={ProgressBarPage} />
-                            <Route path={"/icons"} component={IconPage} />
-                            <Route path={"/carousel"} component={CarouselPage} />
-                            <Route path={"/pagination"} component={PaginationPage} />
-                            <Route path={"/chart"} component={ChartPage} />
-                            <Route path={"/stepper"} component={StepperPage} />
-                            <Route path={"/image"} component={ImagePage} />
-                            <Route path={"/timeline"} component={TimelinePage} />
-                            <Route path={"/steptracker"} component={StepTrackerPage} />
-                            <Route path={"/breadcrumb"} component={BreadcrumbPage} />
-                            <Route path={"/accordion"} component={AccordionPage} />
-                            <Route path={"/inlinelink"} component={InlineLinkPage} />
-                            <Route path={"/tabs"} component={TabsPage} />
-                            <Route path={"/video"} component={VideoPage} />
-                            <Route path={"/notification"} component={NotificationPage} />
-                            <Route path={"/timepicker"} component={TimepickerPage} />
-                            <Route path={"/imagecropper"} component={ImageCropperPage} />
-                            <Route path={"/timer"} component={TimerPage} />
-                            <Route path="*" component={NotFound} props={null} />
-                        </Switch>
-                    </React.Suspense>
-                </div>
-                <SideBar toggle={this.state.sidebarToggle} />
+    return (
+        <div className="app-container">
+            <TitleBar onToggleClick={toggleSidebar} history={props.history} />
+            <div className={"route-holder" + (sidebarToggle ? " sidebar-opened" : "") + (brief ? " brief" : "")}>
+                <React.Suspense fallback={<Loader toggle={true} fullscreen={true} />}>
+                    <Switch>
+                        <Route path="/" exact={true}><Redirect to="/about" /></Route>
+                        {routes.map((item: RouteItem, index: number) => <Route key={index} {...item} />)}
+                    </Switch>
+                </React.Suspense>
             </div>
-        );
-    }
-}
+            <SideBar toggle={sidebarToggle} history={props.history} />
+        </div>
+    );
+};
 
 export default withRouter(App);
--- develop/components/common/SideBar.tsx
+++ develop/components/common/SideBar.tsx
@@ -1,25 +1,35 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
+import * as H from "history";
 
 const sidebarData = require("../../assets/sidebar.json");
-const forms = sidebarData.form.sort((a, b) => { if (a.name < b.name) { return -1; } if (a.name > b.name) { return 1; } return 0; });
-const uis = sidebarData.ui.sort((a, b) => { if (a.name < b.name) { return -1; } if (a.name > b.name) { return 1; } return 0; });
-const others = sidebarData.other.sort((a, b) => { if (a.name < b.name) { return -1; } if (a.name > b.name) { return 1; } return 0; });
+const forms: Array<SideBarItem> = sidebarData.form.sort((a, b) => { if (a.name < b.name) { return -1; } if (a.name > b.name) { return 1; } return 0; });
+const uis: Array<SideBarItem> = sidebarData.ui.sort((a, b) => { if (a.name < b.name) { return -1; } if (a.name > b.name) { return 1; } return 0; });
+const others: Array<SideBarItem> = sidebarData.other.sort((a, b) => { if (a.name < b.name) { return -1; } if (a.name > b.name) { return 1; } return 0; });
+
+type SideBarItem = { name: string, path: string };
 
 interface SideBarProps {
     toggle: boolean;
+    history: H.History;
 }
 
 const SideBar: React.FunctionComponent<SideBarProps> = (props: SideBarProps): React.ReactElement<void> => {
-    const str = location.href.toLowerCase();
-    const page = str.substr(str.lastIndexOf("/"));
+    const page: string = props.history.location.pathname;
+
+    function navigate(e: React.MouseEvent<HTMLAnchorElement>): void {
+        e.preventDefault();
+        if (props.history.location.pathname !== e.currentTarget.getAttribute("href")) {
+            props.history.push(e.currentTarget.getAttribute("href"));
+        }
+    }
 
     return (
         <div className={"side-bar" + (props.toggle ? " open" : "")}>
             <div className="category">
                 <div className="title">How it works?</div>
                 <div className="nav-holder">
-                    <Link to="/about" className={(page === "/" || page === "/about") ? "active" : ""}><span>About</span></Link>
+                    <a href="/about" onClick={navigate} className={(page === "/" || page === "/about") ? "active" : ""}><span>About</span></a>
                     <a href="https://github.com/sebgroup/react-components/issues" target="_blank"><span>Issues</span></a>
                     <a href="https://github.com/sebgroup/react-components/releases" target="_blank"><span>Release notes</span></a>
                 </div>
@@ -27,17 +37,38 @@ const SideBar: React.FunctionComponent<SideBarProps> = (props: SideBarProps): Re
             <div className="category">
                 <div className="title">Components</div>
                 <div className="nav-holder">
-                    <div className="sub-title">Form</div>
-                    {forms && forms.map((item, index) =>
-                        <Link key={index} to={item.path} className={page === item.path ? "active" : ""}><span>{item.name}</span></Link>
+                    {forms && <div className="sub-title">Form</div>}
+                    {forms && forms.map((item: SideBarItem, index: number) =>
+                        <a
+                            key={index}
+                            href={item.path}
+                            onClick={navigate}
+                            className={page === item.path ? "active" : null}
+                        >
+                            <span>{item.name}</span>
+                        </a>
                     )}
-                    <div className="sub-title">UI</div>
-                    {uis && uis.map((item, index) =>
-                        <Link key={index} to={item.path} className={page === item.path ? "active" : ""}><span>{item.name}</span></Link>
+                    {uis && <div className="sub-title">UI</div>}
+                    {uis && uis.map((item: SideBarItem, index: number) =>
+                        <a
+                            key={index}
+                            href={item.path}
+                            onClick={navigate}
+                            className={page === item.path ? "active" : null}
+                        >
+                            <span>{item.name}</span>
+                        </a>
                     )}
-                    <div className="sub-title">Other</div>
-                    {others && others.map((item, index) =>
-                        <Link key={index} to={item.path} className={page === item.path ? "active" : ""}><span>{item.name}</span></Link>
+                    {others && <div className="sub-title">Other</div>}
+                    {others && others.map((item: SideBarItem, index: number) =>
+                        <a
+                            key={index}
+                            href={item.path}
+                            onClick={navigate}
+                            className={page === item.path ? "active" : null}
+                        >
+                            <span>{item.name}</span>
+                        </a>
                     )}
                 </div>
             </div>
