93e074cf35a4ef201751a13fcd3b445015fa816c
Yousif Al-Raheem <yousifalraheem@gmail.com>
2019-11-26 00:57:31 +0800

    docs: converts class doc page to function

--- develop/components/common/DocPage.tsx
+++ develop/components/common/DocPage.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { CheckBox } from "../../../src/CheckBox/CheckBox";
 import { DropdownItem, Dropdown } from "../../../src/Dropdown/Dropdown";
-import { ClassStorage } from "../../utils/ClassStorage";
 
 interface ComponentSpecification<K> {
     type: string;
@@ -19,65 +18,72 @@ type DocPageProps<T> = {
     specifications: ComponentSpecs<T>;
 };
 
-type DocPageState<T> = {
-    componentProps: T;
-    componentAPIs: Array<JSX.Element>;
-};
+type ComponentProps = { [k: string]: ComponentSpecification<any>["content"] };
 
-export class DocPage<T> extends React.Component<DocPageProps<T>, DocPageState<T>> {
-    private storage: ClassStorage = new ClassStorage();
-    constructor(props: DocPageProps<T>) {
-        super(props);
+function DocPage<T>(props: DocPageProps<T>): JSX.Element {
+    const [componentProps, setComponentProps] = React.useState<{ [k: string]: any }>({});
+    const [componentAPIs, setComponentAPIs] = React.useState<Array<JSX.Element>>([]);
 
-        const componentProps: T = {} as unknown as any;
+    React.useEffect(() => setComponentProps(generateComponentProps()), [props.specifications]);
+    React.useEffect(() => setComponentAPIs(generateApisFromSpecs()), [componentProps]);
 
-        for (const prop in this.props.specifications) {
-            if (Array.isArray(this.props.specifications[prop].content)) {
-                componentProps[prop] = this.props.specifications[prop].content[0];
+    /** Generates the component's props */
+    const generateComponentProps = React.useCallback((): ComponentProps => {
+        const newProps: ComponentProps = {};
+
+        for (const specName in props.specifications) {
+            const spec = props.specifications[specName];
+            if (spec.defaultValue !== undefined) {
+                newProps[specName] = spec.defaultValue;
+            } else if (Array.isArray(spec.content)) {
+                newProps[specName] = spec.content[0];
             } else {
-                if (React.isValidElement(this.props.specifications[prop].content)) {
-                    this.storage.setItem(prop, this.props.specifications[prop].content);
+                newProps[specName] = spec.content;
+            }
+        }
+
+        return newProps;
+    }, [props.specifications]);
+
+    /**
+     * Handles form element changes
+     * @param e The change event
+     * @param name The name of the dropdown element changed
+     */
+    const onFormChange = React.useCallback((e: React.ChangeEvent<HTMLInputElement> | DropdownItem, name?: string): void => {
+        if ((e as React.ChangeEvent<HTMLInputElement>).target) {
+            const event: React.ChangeEvent<HTMLInputElement> = e as React.ChangeEvent<HTMLInputElement>;
+            if (event.target.type === "checkbox") {
+                if (event.target.id.search("feature-toggle") !== -1) {
+                    setComponentProps({
+                        ...componentProps,
+                        [event.target.name]: componentProps[event.target.name]
+                            ? null
+                            : (props.specifications[event.target.name] as ComponentSpecification<any>).content
+                    });
+                } else {
+                    setComponentProps({ ...componentProps, [event.target.name]: event.target.checked });
                 }
-                componentProps[prop] = this.props.specifications[prop].content as any;
+            } else {
+                setComponentProps({ ...componentProps, [event.target.name]: event.target.value });
             }
-            if (this.props.specifications[prop].defaultValue !== undefined) {
-                componentProps[prop] = this.props.specifications[prop].defaultValue;
-            }
+        } else if ((e as DropdownItem & object).hasOwnProperty("label") && (e as DropdownItem & object).hasOwnProperty("value")) {
+            const dropdownValue: DropdownItem = e as DropdownItem;
+            setComponentProps({ ...componentProps, [name]: dropdownValue.value });
         }
+    }, [setComponentProps, componentProps]);
 
-        this.state = {
-            componentProps,
-            componentAPIs: []
-        };
-    }
-
-    generateDocumentation = (): Array<JSX.Element> => {
-        const output: Array<JSX.Element> = [];
-        const { specifications } = this.props;
-        let i: number = 0;
-        for (const prop in specifications) {
-            output.push(
-                <tr key={i}>
-                    <td>{prop}</td>
-                    <td><code>{specifications[prop].type}</code></td>
-                    <td>{specifications[prop].description}</td>
-                    <td>{this.getValueControlType(specifications[prop], prop)}</td>
-                </tr>
-            );
-            i++;
-        }
-        return output;
-    }
-
-    getValueControlType = (spec: ComponentSpecification<T[any]>, name: string): React.ReactNode => {
+    /**
+     * Get the control element that will be rendered in the APIs table under `Try it`
+     * @param spec The single specification to be inspected
+     * @param name The name of the form element
+     * @returns The control element as a React node
+     */
+    const getValueControlType = React.useCallback((spec: ComponentSpecification<T[keyof T]>, name: string): React.ReactNode => {
         let output: React.ReactNode = "";
         if (Array.isArray(spec.content)) {
             if (typeof spec.content[0] === "boolean" && typeof spec.content[1] === "boolean") {
-                output = <CheckBox
-                    name={name}
-                    checked={this.state.componentProps[name]}
-                    onChange={this.onFormChange}
-                />;
+                output = <CheckBox name={name} checked={!!componentProps[name]} onChange={onFormChange} />;
             } else if (spec.content.length) {
                 const dropdownList: Array<DropdownItem> = [];
                 spec.content.map((value: any) => {
@@ -85,95 +91,75 @@ export class DocPage<T> extends React.Component<DocPageProps<T>, DocPageState<T>
                 });
                 output = <Dropdown
                     name={name}
-                    selectedValue={{ label: this.state.componentProps[name], value: this.state.componentProps[name] }}
-                    onChange={(value: DropdownItem) => this.onFormChange(value, name)}
+                    selectedValue={{ label: componentProps[name], value: componentProps[name] }}
+                    onChange={(val: DropdownItem) => onFormChange(val, name)}
                     list={dropdownList}
                 />;
             }
         } else {
             if (React.isValidElement(spec.content)) {
-                output = <CheckBox
-                    name={name}
-                    id={"feature-toggle-" + name}
-                    checked={!!this.state.componentProps[name]}
-                    onChange={this.onFormChange}
-                />;
+                output = <CheckBox name={name} id={"feature-toggle-" + name} checked={!!componentProps[name]} onChange={onFormChange} />;
             }
         }
         return output;
-    }
+    }, [setComponentProps, componentProps, props.specifications]);
 
-    onFormChange = (e: React.ChangeEvent<HTMLInputElement> | DropdownItem, name?: string): void => {
-        if ((e as React.ChangeEvent<HTMLInputElement>).target) {
-            const event: React.ChangeEvent<HTMLInputElement> = e as React.ChangeEvent<HTMLInputElement>;
-            if (event.target.type === "checkbox") {
-                if (event.target.id.search("feature-toggle") !== -1) {
-                    this.setState({
-                        componentProps: {
-                            ...this.state.componentProps,
-                            [event.target.name]: this.state.componentProps[event.target.name]
-                                ? null
-                                : this.storage.getItem(event.target.name)
-                        }
-                    });
-                } else {
-                    this.setState({ componentProps: { ...this.state.componentProps, [event.target.name]: event.target.checked } });
-                }
-            } else {
-                this.setState({ componentProps: { ...this.state.componentProps, [event.target.name]: event.target.value } });
-            }
-        } else if ((e as DropdownItem & object).hasOwnProperty("label") && (e as DropdownItem & object).hasOwnProperty("value")) {
-            const dropdownValue: DropdownItem = e as DropdownItem;
-            this.setState({ componentProps: { ...this.state.componentProps, [name]: dropdownValue.value } });
+    /**
+     * Generates the APIs table using the specifications
+     * @returns The table rows as an array of `JSX elements`
+     */
+    const generateApisFromSpecs = React.useCallback((): Array<JSX.Element> => {
+        const output: Array<JSX.Element> = [];
+        for (const specName in props.specifications) {
+            const spec: ComponentSpecification<any> = props.specifications[specName];
+            output.push(
+                <tr key={specName}>
+                    <td>{specName}</td>
+                    <td><code>{spec.type}</code></td>
+                    <td>{spec.description}</td>
+                    <td>{getValueControlType(spec, specName)}</td>
+                </tr>
+            );
         }
-    }
+        return output;
+    }, [props.specifications, getValueControlType]);
 
-    componentDidMount() {
-        this.setState({ componentAPIs: this.generateDocumentation() });
-    }
-
-    componentDidUpdate(prevProps: DocPageProps<T>, prevState: DocPageState<T>) {
-        if (prevState.componentProps && prevState.componentProps !== this.state.componentProps) {
-            this.setState({ componentAPIs: this.generateDocumentation() });
-        }
-    }
-
-    render() {
-        return (
-            <section className="doc-page">
-                <h1>{this.props.header}</h1>
-                <p>{this.props.description}</p>
-                <div className="row">
-                    <div className="col-12">
-                        <div className="card">
-                            <h4>Playground</h4>
-                            <div className="output">
-                                <this.props.component {...this.state.componentProps} />
-                            </div>
+    return (
+        <section className="doc-page">
+            <h1>{props.header}</h1>
+            <p>{props.description}</p>
+            <div className="row">
+                <div className="col-12">
+                    <div className="card">
+                        <h4>Playground</h4>
+                        <div className="output">
+                            <props.component {...componentProps as any} />
                         </div>
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-12">
-                        <div className="card">
-                            <h4>APIs</h4>
-                            <table className="table table-bordered">
-                                <thead className="thead-dark">
-                                    <tr>
-                                        <th>Propery</th>
-                                        <th>Type</th>
-                                        <th>Description</th>
-                                        <th>Try it</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {this.state.componentAPIs}
-                                </tbody>
-                            </table>
-                        </div>
+            </div>
+            <div className="row">
+                <div className="col-12">
+                    <div className="card">
+                        <h4>APIs</h4>
+                        <table className="table table-bordered">
+                            <thead className="thead-dark">
+                                <tr>
+                                    <th>Propery</th>
+                                    <th>Type</th>
+                                    <th>Description</th>
+                                    <th>Try it</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {componentAPIs}
+                            </tbody>
+                        </table>
                     </div>
                 </div>
-            </section>
-        );
-    }
+            </div>
+        </section>
+    );
 }
+
+export { DocPage };
--- develop/utils/ClassStorage.ts
+++
@@ -1,8 +0,0 @@
-export class ClassStorage {
-    setItem(name: string, value: any): void {
-        this[name] = value;
-    }
-    getItem(name: string): any {
-        return this[name];
-    }
-}
