eeff6cdb5eafa3d6e4982e3aa5be6f911222e88e
Yousif Al-Raheem <yousifalraheem@gmail.com>
2020-05-14 17:20:54 +0800

    feat(feedbackindicator): new component FeedbackIndicator
    New component used to wrap other elements with a feedback box

---
+++ develop/components/pages/FeedbackIndicatorPage.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { FeedbackIndicator, IndicatorType } from "../../../src/FeedbackIndicator/FeedbackIndicator";
+import Highlight from "react-highlight";
+import { CheckBox } from "../../../src/CheckBox/CheckBox";
+import { Button } from "../../../src/Button";
+const docMD: string = require("../../../src/FeedbackIndicator/readme.md");
+
+const FeedbackIndicatorPage: React.FC = () => {
+    const [message, setMessage] = React.useState<string>("");
+    const [errorType, setErrorType] = React.useState<IndicatorType>(null);
+    const [acceptConditions, setAcceptConditions] = React.useState<boolean>(false);
+    const [acceptTerms, setAcceptTerms] = React.useState<boolean>(false);
+
+    const handleSubmit = React.useCallback(
+        (e: React.FormEvent<HTMLFormElement>): void => {
+            e.preventDefault();
+            switch (+acceptTerms + +acceptConditions) {
+                case 0:
+                    setMessage("You have to accept the terms and conditions");
+                    setErrorType("danger");
+                    break;
+                case 1:
+                    setMessage("Both terms and conditions must be accepted");
+                    setErrorType("warning");
+                    break;
+                case 2:
+                    setMessage("You're all set!");
+                    setErrorType("success");
+                    break;
+            }
+        },
+        [acceptTerms, acceptConditions]
+    );
+
+    const handleReset = React.useCallback((e: React.FormEvent<HTMLFormElement>): void => {
+        e.preventDefault();
+        setMessage("");
+        setErrorType(null);
+        setAcceptConditions(false);
+        setAcceptTerms(false);
+    }, []);
+
+    return (
+        <div className="route-template container">
+            <div className="info-holder">
+                <div className="info">
+                    <div className="md-file">
+                        <Highlight innerHTML={true}>{docMD}</Highlight>
+                    </div>
+                </div>
+
+                <div className="info">
+                    <h3 className="border-bottom pb-2">Output</h3>
+                    <p>Make different combinations of submissions to see the indicator changes</p>
+                    <div className="result">
+                        <form onSubmit={handleSubmit} onReset={handleReset}>
+                            <FeedbackIndicator className="mb-4" type={errorType} message={message}>
+                                <div>
+                                    <CheckBox label="Accept conditions" checked={acceptConditions} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setAcceptConditions(e.target.checked)} />
+                                    <CheckBox label="Accept terms" checked={acceptTerms} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setAcceptTerms(e.target.checked)} />
+                                </div>
+                            </FeedbackIndicator>
+                            <Button type="submit" className="mr-2">
+                                Submit
+                            </Button>
+                            <Button type="reset" theme="outline-primary">
+                                Reset
+                            </Button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FeedbackIndicatorPage;
---
+++ src/FeedbackIndicator/FeedbackIndicator.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import classnames from "classnames";
+import "./feedback-indicator.scss";
+
+export type IndicatorType = "danger" | "warning" | "success";
+export type Indicator = {
+    /** The type of the indicator. Available values: "danger" | "warning" | "success" */
+    type: IndicatorType;
+    /** The indicator message. `children` can also be used instead. */
+    message?: React.ReactNode;
+};
+
+export type FeedbackIndicatorProps = JSX.IntrinsicElements["div"] & Indicator;
+
+export const FeedbackIndicator: React.FC<FeedbackIndicatorProps> = React.memo(({ type, message, children, ...props }: FeedbackIndicatorProps) => {
+    const [indicatorValue, setIndicatorValue] = React.useState<number>(0);
+
+    React.useEffect(() => {
+        switch (type) {
+            case "danger":
+                setIndicatorValue(10);
+                break;
+            case "warning":
+                setIndicatorValue(50);
+                break;
+            case "success":
+                setIndicatorValue(100);
+                break;
+            default:
+                setIndicatorValue(0);
+        }
+    }, [type]);
+
+    return (
+        <div {...props} className={classnames("seb", "progress-feedback", `progress-${indicatorValue}`, { "wrapper-indicator": children }, props.className)}>
+            {children && <div className="children">{children}</div>}
+            {!!indicatorValue && message}
+        </div>
+    );
+});
