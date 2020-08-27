00ee82c958aa2e97978afac54a181f6418b17461
Salihu Abdullahi <S4060B@corp1.ad1.seb.net>
2020-08-27 15:45:35 +0800

    feat(table): added inline column custom support

--- develop/__utils/makeData.tsx
+++ develop/__utils/makeData.tsx
@@ -1,46 +1,51 @@
 import React from "react";
 import { loremIpsum } from "lorem-ipsum";
 import { randomId } from "@sebgroup/frontend-tools/dist/randomId";
+import { ActionLinkItem, TableRow } from "../../src/Table/Table";
 
 function range(len: number): Array<number> {
-  const arr: Array<number> = [];
-  for (let i = 0; i < len; i++) {
-    arr.push(i);
-  }
-  return arr;
-};
+    const arr: Array<number> = [];
+    for (let i = 0; i < len; i++) {
+        arr.push(i);
+    }
+    return arr;
+}
 
 function newPerson(): object {
-  const statusChance: number = Math.random();
-  return {
-    id: parseInt(randomId("").substr(8, 4), 10),
-    firstName: loremIpsum({ units: "words", count: 1 }),
-    lastName: loremIpsum({ units: "words", count: 1 }),
-    age: Math.floor(Math.random() * 30),
-    visits: Math.floor(Math.random() * 100),
-    progress: Math.floor(Math.random() * 100),
-    status:
-      statusChance > 0.66
-        ? "relationship"
-        : statusChance > 0.33
-          ? "complicated"
-          : "single",
-  };
-};
+    const statusChance: number = Math.random();
+    return {
+        id: parseInt(randomId("").substr(8, 4), 10),
+        firstName: loremIpsum({ units: "words", count: 1 }),
+        lastName: loremIpsum({ units: "words", count: 1 }),
+        age: Math.floor(Math.random() * 30),
+        visits: Math.floor(Math.random() * 100),
+        progress: Math.floor(Math.random() * 100),
+        status: statusChance > 0.66 ? "relationship" : statusChance > 0.33 ? "complicated" : "single",
+    };
+}
 
-export default function makeData<T>(lens: Array<number>): T {
-  const makeDataLevel: Function = (depth: number = 0): object | Function => {
-    const len: number = lens[depth]
-    return range(len).map((d: number) => {
-      return {
-        ...newPerson(),
-        subRows: lens[depth + 1] ? makeDataLevel(depth + 1) : undefined,
-        rowContentDetail: <p className="details">
-          {loremIpsum({ units: "sentences", count: 2 })}
-        </p>
-      }
-    })
-  }
+const actionLinks: Array<ActionLinkItem> = [
+    {
+        label: "Edit",
+        onClick: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>, selectedRow: TableRow) => {
+            console.log("The selectedrow is ", selectedRow);
+        },
+    },
+];
 
-  return makeDataLevel();
+export default function makeData<T>(lens: Array<number>, useRowActionColumn?: boolean): T {
+    const makeDataLevel: Function = (depth: number = 0): object | Function => {
+        const len: number = lens[depth];
+        return range(len).map((d: number) => {
+            return {
+                ...newPerson(),
+                subRows: lens[depth + 1] ? makeDataLevel(depth + 1) : undefined,
+                actionLinks: useRowActionColumn ? (d % 2 === 0 ? actionLinks : null) : null,
+                actionButtonState: useRowActionColumn ? (d % 2 === 0 ? "disabled" : null) : null,
+                rowContentDetail: <p className="details">{loremIpsum({ units: "sentences", count: 2 })}</p>,
+            };
+        });
+    };
+
+    return makeDataLevel();
 }
