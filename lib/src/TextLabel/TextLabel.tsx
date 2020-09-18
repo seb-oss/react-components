e2bb02a190b4ddc2cf3c61324bd92c2a7f5d75cb
Chang <S2721C@corp1.ad1.seb.net>
2020-09-18 14:41:16 +0800

    docs(textlabel): add doc for text label

--- lib/src/TextLabel/TextLabel.tsx
+++ lib/src/TextLabel/TextLabel.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import "./text-label-style.scss";
 
 export interface TextLabelProps {
+    /** Optional custom class to append to the modal. */
     className?: string;
+    /**	Optional id for the textLabel. */
     id?: string;
+    /** Optional label for the textLabel can be a string or a template. */
     label?: string | React.ReactNode;
+    /** Optional label for the text label can be a string or a template. */
     value: string | number | React.ReactNode;
 }
 
+/** A text label is a component to display value with label */
 export const TextLabel: React.FC<TextLabelProps> = React.memo((props: TextLabelProps) => {
     return (
         <div className={"text-label" + (props.className ? ` ${props.className}` : "")} id={props.id}>
