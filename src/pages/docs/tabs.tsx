6a8b4606f6bacbaf3097d26a801c304d4a7277ec
Yousif Al-Raheem <yousifalraheem@gmail.com>
2020-10-16 09:07:21 +0800

    docs(tabs): fixed tabs docs

--- src/pages/docs/tabs.tsx
+++ src/pages/docs/tabs.tsx
@@ -7,7 +7,7 @@ const TabsPage: React.FC = React.memo(() => {
     const importString: string = require("!raw-loader!@sebgroup/react-components/Tabs/Tabs");
     const [active, setActive] = React.useState<number>(0);
     const [usePills, setUsePills] = React.useState<boolean>(false);
-    const fields: Array<DynamicFormSection> = [
+    const [renderControls, form, setForm] = useDynamicForm([
         {
             key: "controls",
             items: [
@@ -27,8 +27,7 @@ const TabsPage: React.FC = React.memo(() => {
                 },
             ],
         },
-    ];
-    const [renderForm, form, setForm] = useDynamicForm(fields);
+    ]);
     const code: string = `
 <Tabs value={value} onValueChange={setValue}>
     <TabItem>First</TabItem>
@@ -38,7 +37,7 @@ const TabsPage: React.FC = React.memo(() => {
 </Tabs>
 `;
 
-    React.useEffect(() => setActive(form.controls?.active), [form.controls?.active]);
+    React.useEffect(() => setActive(form.controls.active), [form.controls.active]);
 
     React.useEffect(() => {
         if (active !== form.controls.active) {
@@ -50,7 +49,7 @@ const TabsPage: React.FC = React.memo(() => {
         <Docs
             mainFile={importString}
             example={
-                <Tabs value={active} onValueChange={setActive} usePills={usePills}>
+                <Tabs value={form.controls.active} onValueChange={setActive} usePills={form.controls.usePills}>
                     <TabItem>First</TabItem>
                     <TabItem>Second</TabItem>
                     <TabItem>Third</TabItem>
@@ -58,7 +57,7 @@ const TabsPage: React.FC = React.memo(() => {
                 </Tabs>
             }
             code={code}
-            controls={renderForm()}
+            controls={renderControls()}
         />
     );
 });
