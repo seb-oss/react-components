bba0678b14b39e45b55273db57a18b5164683450
Chang <S2721C@corp1.ad1.seb.net>
2020-11-17 15:40:01 +0800

    refactor(textlabel): spread props in textlabel

--- lib/src/TextLabel/TextLabel.tsx
+++ lib/src/TextLabel/TextLabel.tsx
@@ -2,23 +2,19 @@ import React from "react";
 import "./text-label-style.scss";
 import classnames from "classnames";
 
-export interface TextLabelProps {
-    /** Optional custom class to append to the modal. */
-    className?: string;
-    /**	Optional id for the textLabel. */
-    id?: string;
+export type TextLabelProps = JSX.IntrinsicElements["div"] & {
     /** Optional label for the textLabel can be a string or a template. */
     label?: string | React.ReactNode;
     /** Optional label for the text label can be a string or a template. */
     value: string | number | React.ReactNode;
-}
+};
 
 /** A text label is a component to display value with label */
-export const TextLabel: React.FC<TextLabelProps> = React.memo((props: TextLabelProps) => {
+export const TextLabel: React.FC<TextLabelProps> = React.memo(({ label, value, ...props }: TextLabelProps) => {
     return (
-        <div className={classnames("text-label", props.className)} id={props.id}>
-            {props.label && <label className="custom-label">{props.label}</label>}
-            <div className="custom-label-value">{props.value}</div>
+        <div className={classnames("text-label", props.className)} {...props}>
+            {label && <label className="custom-label">{label}</label>}
+            <div className="custom-label-value">{value}</div>
         </div>
     );
 });
