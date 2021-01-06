52429bcb6270acf67a46ea7bfa3fc6280b089059
Yousif Al-Raheem <yousifalraheem@gmail.com>
2021-01-06 16:37:48 +0800

    refactor(modal): refactored modal component to declarative style

--- docs/src/pages/docs/modal.tsx
+++ docs/src/pages/docs/modal.tsx
@@ -1,100 +1,145 @@
 import React from "react";
 import Docs from "@common/Docs";
-import { DynamicFormOption, DynamicFormSection, useDynamicForm } from "@hooks/useDynamicForm";
+import { DynamicFormOption, useDynamicForm } from "@hooks/useDynamicForm";
 import { Button } from "@sebgroup/react-components/Button";
-import { Modal } from "@sebgroup/react-components/Modal";
+import { Modal, ModalSize, ModalPosition } from "@sebgroup/react-components/Modal";
+import { Textbox } from "@sebgroup/react-components/Textbox";
+import { CodeSnippet } from "@common/CodeSnippet";
+
+const importString: string = require("!raw-loader!@sebgroup/react-components/Modal/Modal");
+const code: string = `<Modal toggle={toggle}>
+    <div className="modal-header">Header content</div>
+    <div className="modal-body">Modal content here</div>
+    <div className="modal-footer">Footer content</div>
+</Modal>`;
+
+const sizses: Array<DynamicFormOption<ModalSize>> = [
+    { label: "sm", value: "sm", key: "sm" },
+    { label: "md", value: "md", key: "md" },
+    { label: "lg", value: "lg", key: "lg" },
+];
+const positions: Array<DynamicFormOption<ModalPosition>> = [
+    { label: "default", value: "default", key: "default" },
+    { label: "left", value: "left", key: "left" },
+    { label: "right", value: "right", key: "bottom" },
+];
 
 const NotificationPage: React.FC = () => {
-    const importString: string = require("!raw-loader!@sebgroup/react-components/Modal/Modal");
     const [toggle, setToggle] = React.useState<boolean>(false);
-    const modalSizeList: Array<DynamicFormOption> = [
-        { label: "default", value: "default", key: "default" },
-        { label: "modal-sm", value: "modal-sm", key: "modal-sm" },
-        { label: "modal-lg", value: "modal-lg", key: "modal-lg" },
-    ];
-    const positionList: Array<DynamicFormOption> = [
-        { label: "default", value: "default", key: "default" },
-        { label: "left", value: "left", key: "left" },
-        { label: "right", value: "right", key: "bottom" },
-    ];
-    const defaultCheckboxControls: Array<DynamicFormOption> = [
-        { label: "centered", value: "centered", key: "centered" },
-        { label: "fullscreen", value: "fullscreen", key: "fullscreen" },
-        { label: "disable backdrop dismiss", value: "disablebackdropdismiss", key: "disablebackdropdismiss" },
-        { label: "escape to dismiss", value: "escapeToDismiss", key: "escapeToDismiss" },
-        { label: "trap focus", value: "trapfocus", key: "trapfocus" },
-    ];
-    const fields: Array<DynamicFormSection> = [
+
+    const [renderForm, { controls }] = useDynamicForm([
         {
             key: "controls",
             items: [
-                {
-                    key: "size",
-                    value: modalSizeList[0].value,
-                    label: "Size",
-                    options: modalSizeList,
-                    controlType: "Dropdown",
-                },
-                {
-                    key: "position",
-                    value: positionList[0],
-                    label: "Position",
-                    options: positionList,
-                    controlType: "Dropdown",
-                },
-                {
-                    label: "Optional configurations",
-                    key: "checkboxes",
-                    controlType: "Option",
-                    options: defaultCheckboxControls,
-                },
+                { key: "size", label: "Size", controlType: "Radio", options: sizses, value: sizses[1], inline: true },
+                { key: "position", label: "Position", controlType: "Radio", options: positions, value: positions[0], inline: true },
+                { key: "centered", label: "centered", controlType: "Checkbox", value: false },
+                { key: "fullscreen", label: "fullscreen", controlType: "Checkbox", value: false },
+                { key: "trapfocus", label: "trapfocus", controlType: "Checkbox", value: false },
+                { key: "autoFocus", label: "autoFocus", description: "Automatically focus on the first input element", controlType: "Checkbox", value: false },
+                { key: "onEscape", label: "onEscape", controlType: "Checkbox", value: false },
+                { key: "onBackdropDismiss", label: "onBackdropDismiss", controlType: "Checkbox", value: false },
             ],
         },
-    ];
-    const [renderForm, { controls }] = useDynamicForm(fields);
-    const code: string = `<Modal
-        toggle={this.state.dialogue}
-        fullscreen={this.state.fullscreen}
-        position={this.state.position}
-        disableBackdropDismiss={this.state.disableBackdropDismiss}
-        onDismiss={() => this.closeModal()}
-        header={<h3>Header</h3>}
-        body={<p>this is the body</p>}
-        footer={<Button
-            label="Close Modal"
-            onClick={() => this.closeModal()}
-        />}
-    />`;
+    ]);
 
-    /** check if key selected */
-    const checkSelectedKey = (key: string) => {
-        return controls.checkboxes?.some((item: DynamicFormOption) => item.key === key);
-    };
+    const dismiss = React.useCallback(() => setToggle(false), []);
 
     return (
         <Docs
             mainFile={importString}
             example={
                 <>
-                    <Button onClick={() => setToggle(true)}>Toggle Modal</Button>
+                    <Button onClick={() => setToggle(!toggle)}>Toggle Modal</Button>
                     <Modal
                         toggle={toggle}
-                        fullscreen={checkSelectedKey("fullscreen")}
-                        position={(controls as any)?.position === "default" ? null : (controls as any)?.position}
-                        size={(controls as any)?.size === "default" ? null : (controls as any)?.size}
-                        disableBackdropDismiss={checkSelectedKey("disablebackdropdismiss")}
-                        escapeToDismiss={checkSelectedKey("escapeToDismiss")}
-                        centered={checkSelectedKey("centered")}
-                        trapFocus={checkSelectedKey("trapfocus")}
-                        onDismiss={() => setToggle(false)}
-                        header={<h3>Header</h3>}
-                        body={<p>this is the body</p>}
-                        footer={<Button onClick={() => setToggle(false)}>Close Modal</Button>}
-                    />
+                        position={controls.position?.value}
+                        size={controls.size?.value}
+                        fullscreen={controls.fullscreen}
+                        centered={controls.centered}
+                        trapFocus={controls.trapfocus}
+                        onEscape={controls.onEscape ? dismiss : null}
+                        onBackdropDismiss={controls.onBackdropDismiss ? dismiss : null}
+                        autoFocus={controls.autoFocus}
+                    >
+                        <div className="modal-header">
+                            <h3>Header</h3>
+                            <button className="close" type="button" onClick={dismiss} />
+                        </div>
+                        <div className="modal-body">
+                            {controls.trapfocus || controls.autoFocus ? (
+                                <form>
+                                    <fieldset>
+                                        <legend>Use tab to see focus trap in action</legend>
+                                        <Textbox label="Firstname" />
+                                        <Textbox label="Lastname" />
+                                        <Button type="button">Submit</Button>
+                                    </fieldset>
+                                </form>
+                            ) : (
+                                <>
+                                    <p>
+                                        Voluptate beatae quo est perferendis quam ut illum repellat voluptatem. Saepe deleniti voluptas impedit quidem ut rerum. Voluptatibus laboriosam sit libero
+                                        deleniti accusamus debitis quia nobis alias. Quod voluptatem id et consequatur suscipit odio cumque vero ut. Ut ullam nisi qui. Doloremque libero nihil omnis
+                                        porro nihil quo.
+                                    </p>
+                                    <p>
+                                        Maxime recusandae cum. Quisquam facere quia corporis debitis. Illo itaque vel magni est dolores ut voluptatem nisi ullam. Ea nihil ipsa et dolor non illum iste
+                                        quis. Sunt dolores sunt non porro.
+                                    </p>
+                                </>
+                            )}
+                        </div>
+                        <div className="modal-footer">
+                            <Button onClick={dismiss}>Close Modal</Button>
+                        </div>
+                    </Modal>
                 </>
             }
             code={code}
             controls={renderForm()}
+            note={
+                <>
+                    <h3>Modal header, body and footer</h3>
+                    <p>
+                        Defining the modal's header, body and footer is done in a declarative style. Meaning that the developer is responsive for declaring these parts individually if needed. Remember
+                        to pass the following class names:
+                    </p>
+                    <ul>
+                        <li>
+                            <code>modal-header</code> for the <code>div</code> representing the header
+                        </li>
+                        <li>
+                            <code>modal-body</code> for the <code>div</code> representing the body
+                        </li>
+                        <li>
+                            <code>modal-footer</code> for the <code>div</code> representing the footer
+                        </li>
+                    </ul>
+                    An example can be found below:
+                    <CodeSnippet language="jsx">
+                        {`<Modal>
+    <div className="modal-header">Header content here</div>
+    <div className="modal-body">Body content here</div>
+    <div className="modal-footer">Footer content here</div>
+</Modal>`}
+                    </CodeSnippet>
+                    <hr />
+                    <h3>Header close button</h3>
+                    <p>
+                        To add a close button to the modal's header just pass a button of class "close" and it will be aligned correctly. You don't need to pass any content inside the button as it
+                        will be populated automatically with an &times; sign.
+                    </p>
+                    <CodeSnippet language="jsx">
+                        {`<Modal>
+    <div className="modal-header">
+        <h3>Modal header text</h3>
+        <button className="close" type="button" onClick={dismiss} />
+    </div>
+</Modal>`}
+                    </CodeSnippet>
+                </>
+            }
         />
     );
 };
