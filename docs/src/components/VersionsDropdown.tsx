2b397dd0841fc6777c5f2aa086751cd3f7eea93b
Kher Phay Chang <kayphay92@gmail.com>
2021-03-20 17:57:20 +0800

    docs(doc): modify version dropdown to show .x for patch

--- docs/src/components/VersionsDropdown.tsx
+++ docs/src/components/VersionsDropdown.tsx
@@ -3,18 +3,39 @@ import { Dropdown } from "@sebgroup/react-components/Dropdown";
 import { withPrefix } from "gatsby-link";
 const versions = require("../assets/jsons/versions.json").include;
 
+interface VersionItem {
+    name: string;
+}
+interface FormattedVersionItem {
+    value: string;
+    label: string;
+}
+
 export interface VersionsDropdownProps {
     className?: string;
 }
 
 const VersionsDropdown: React.FC<React.PropsWithChildren<VersionsDropdownProps>> = (props: VersionsDropdownProps) => {
     const [currentVersion, setCurrentVersion] = React.useState<string>("");
+    const [formattedList, setFormattedList] = React.useState<Array<FormattedVersionItem>>([]);
+
+    const formatList = () => {
+        setFormattedList(
+            versions?.length > 0
+                ? versions.map((item: VersionItem) => ({
+                      value: item.name,
+                      label: item.name.replace(/[^.]+$/, "x"),
+                  }))
+                : []
+        );
+    };
 
     React.useEffect(() => {
         const currentpath: string = window.location.href;
         const regex: RegExp = new RegExp("v(\\d+\\.)(\\d+\\.)(\\d)(-(beta|alpha)(\\.\\d))?", "g");
         const matchedVersions: Array<string> = currentpath.match(regex);
         setCurrentVersion(matchedVersions?.length > 0 ? matchedVersions[0] : versions[0].name);
+        formatList();
     }, []);
 
     return (
@@ -23,9 +44,9 @@ const VersionsDropdown: React.FC<React.PropsWithChildren<VersionsDropdownProps>>
             value={currentVersion}
             onChange={(e: React.ChangeEvent<HTMLSelectElement>) => (window.location.href = `${window.location.origin}${withPrefix(e.target.value)}/`)}
         >
-            {versions.map((item, index: number) => (
-                <option key={index} value={item.name}>
-                    {item.name}
+            {formattedList.map((item: FormattedVersionItem, index: number) => (
+                <option key={index} value={item.value}>
+                    {item.label}
                 </option>
             ))}
         </Dropdown>
