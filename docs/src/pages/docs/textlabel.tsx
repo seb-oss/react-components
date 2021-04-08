76362294459f17f7b9ebbc2159169d035950a379
Mario Subotic <mario.subotic@gmail.com>
2021-04-08 20:07:57 +0800

    docs(dynamic-form): fix option controls in docs

--- docs/src/pages/docs/textlabel.tsx
+++ docs/src/pages/docs/textlabel.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Docs from "@common/Docs";
 import { TextLabel } from "@sebgroup/react-components/TextLabel";
-import { DynamicFormOption, DynamicFormSection, useDynamicForm } from "@sebgroup/react-components/hooks/useDynamicForm";
+import { DynamicFormSection, useDynamicForm } from "@sebgroup/react-components/hooks/useDynamicForm";
 
 const TextLabelPage: React.FC = React.memo(() => {
     const importString: string = require("!raw-loader!@sebgroup/react-components/TextLabel/TextLabel");
@@ -23,7 +23,7 @@ const TextLabelPage: React.FC = React.memo(() => {
 
     /** check if key selected */
     const checkSelectedKey = (key: string) => {
-        return controls.checkboxes?.some((item: DynamicFormOption) => item.key === key);
+        return controls.checkboxes?.find((item: string) => item === key);
     };
 
     return (
