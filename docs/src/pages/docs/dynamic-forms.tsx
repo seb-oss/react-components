ef7fed03421d7cdbe353cafafcc79526b5e56a88
Mario Subotic <mario.subotic@gmail.com>
2021-05-24 17:00:05 +0800

    Revert "fix(dynamic-forms): rename touched to dirty"
    This reverts commit 122c1e39d78b9c660aa7c0979902df88e66ad82b.

--- docs/src/pages/docs/dynamic-forms.tsx
+++ docs/src/pages/docs/dynamic-forms.tsx
@@ -14,11 +14,311 @@ const DynamicForms: React.FC = React.memo(() => {
                 <title>Dynamic Forms - SEB React Components</title>
             </Helmet>
             <div className="container">
+                <h1 className="pt-5 pb-3">Dynamic Forms</h1>
+                <h4 className="font-weight-normal">How to use Dynamic Forms Custom Hook to speed up building simple forms.</h4>
+
+                <div role="alert" className="alert alert-warning rounded mt-3 py-3">
+                    <strong>⚠ Dynamic Forms custom hook is still in beta stage so expect things to change (please provide feedback if you can)</strong>
+                </div>
+
+                <h2 className="pt-3 pb-3">Basic Usage</h2>
+                <p>
+                    Import the hook, define the <code>DynamicFormSection</code>s and render the form.
+                </p>
+                <CodeSnippet language="javascript">
+                    {`
+import { useDynamicForm } from "@sebgroup/react-components/hooks/useDynamicForm";
+
+const Component = () => {
+  const sections = [
+    {
+      title: "Login",
+      key: "section-1-login",
+      items: [
+        {
+          key: "name",
+          label: "Name",
+          order: 1,
+          controlType: "Text"
+        },
+        {
+          key: "email",
+          label: "Email",
+          placeholder: "name@domain.com",
+          order: 2,
+          controlType: "Text"
+        },
+        {
+          key: "user-accepted",
+          label: "I understand",
+          order: 3,
+          value: true,
+          controlType: "Checkbox"
+        }
+      ]
+    }
+  ];
+
+  const [renderForm, state] = useDynamicForm(sections);
+
+  // the state of the form with all the current values
+  console.log(state)
+
+  return <div>{renderForm()}</div>;
+};
+
+export default Component;
+                    `}
+                </CodeSnippet>
+
+                <div className="p-3 rounded bg-white">
+                    <Component />
+                </div>
+
+                <hr />
+
+                <h2 className="pt-3 pb-3">Conditional rendering example</h2>
+                <p>
+                    Define the <b>condition</b> and the <b>rulerKey</b> of the <code>DynamicFormItem</code> to render that item based on the value of another item.
+                </p>
+                <CodeSnippet language="javascript">
+                    {`
+import { useDynamicForm } from "@sebgroup/react-components/hooks/useDynamicForm";
+
+const ComponentConditionalRender: React.FC = () => {
+    const sections: DynamicFormSection[] = [
+        {
+            title: "Extra Info",
+            key: "section-2-extra-info",
+            items: [
+                {
+                    key: "have-additional-info",
+                    label: "I have additional information",
+                    order: 1,
+                    value: false,
+                    controlType: "Checkbox"
+                },
+                {
+                    key: "info",
+                    order: 2,
+                    placeholder: "Additional information",
+                    controlType: "Text",
+                    // this component will be shown only when 
+                    // "have-additional-info" component's value is true
+                    rulerKey: "have-additional-info",
+                    condition: true
+                }
+            ],
+        },
+    ];
+
+    const [renderForm, state] = useDynamicForm(sections);
+
+    return <div>{renderForm()}</div>;
+};
+
+export default ComponentConditionalRender;
+                    `}
+                </CodeSnippet>
+                <div className="p-3 rounded bg-white">
+                    <ComponentConditionalRender />
+                </div>
+
+                <hr />
+
+                <h2 className="pt-3 pb-3">Form errors example</h2>
+                <p>Every form element defined can display an error message based on any validation rules you define yourself. Simply set the errors for that key as shown in the code snippet below.</p>
+                <CodeSnippet language="javascript">
+                    {`
+import { useDynamicForm } from "@sebgroup/react-components/hooks/useDynamicForm";
+import { isEmpty } from "@sebgroup/frontend-tools";
+
+const FormWithErrors: React.FC = () => {
+    const sections: DynamicFormSection[] = [
+        {
+            key: "section-3-errors",
+            items: [
+                {
+                    key: "normal",
+                    label: "Non mandatory field",
+                    order: 1,
+                    value: false,
+                    controlType: "Checkbox",
+                },
+                {
+                    key: "with-error",
+                    order: 2,
+                    label: "Mandatory field",
+                    controlType: "Text",
+                },
+            ],
+        },
+    ];
+
+    const [renderForm, state, , setErrors] = useDynamicForm(sections);
+
+    const validate = () => {
+        if (!isEmpty(state) && !isEmpty(state["section-3-errors"])) {
+            setErrors((existing) => {
+                return {
+                    ...existing,
+                    "section-3-errors": {
+                        ...existing["section-3-errors"],
+                        "with-error": !state["section-3-errors"]["with-error"] ? "Please fill in this field" : null,
+                    },
+                };
+            });
+        }
+    };
+
+    return (
+        <>
+            <div>{renderForm()}</div>
+            <Button className="mt-3" onClick={validate}>Validate</Button>
+        </>
+    );
+};
+
+export default FormWithErrors;
+                    `}
+                </CodeSnippet>
+                <div className="p-3 rounded bg-white">
+                    <FormWithErrors />
+                </div>
+
+                <hr />
+
                 <h2 className="pt-3 pb-3">Full example</h2>
                 <p>A complete example of every form element with label, description and optional error message. All form elements use responsive layout.</p>
                 <CodeSnippet language="javascript">
                     {`
 import { useDynamicForm, DynamicFormSection, DynamicFormItem, DynamicFormOption } from "@sebgroup/react-components/hooks";
+
+const ShowMeEverything: React.FC = () => {
+    const COMMON_ITEM: Partial<DynamicFormItem> = {
+        wrappingElement: "div",
+        additionalProps: {
+            className: "col-12 col-sm-6 col-md-4 mb-2",
+        },
+    };
+
+    const valueItems: DynamicFormItem[] = (["Text", "Textarea", "Datepicker", "Stepper", "Checkbox"] as DynamicFormItem["controlType"][]).map(
+        (controlType: DynamicFormItem["controlType"], i: number) => {
+            return {
+                key: controlType,
+                label: \`\${controlType} label\`,
+                description: \`\${controlType} description\`,
+                order: i,
+                controlType,
+                ...COMMON_ITEM,
+            };
+        }
+    );
+
+    const options: DynamicFormOption[] = [
+        { key: "one", label: "One", value: "1" },
+        { key: "two", label: "Two", value: "2" },
+        { key: "three", label: "Three", value: "3" },
+    ];
+
+    const multiSingleItems: DynamicFormItem[] = (["Radio", "Dropdown"] as DynamicFormItem["controlType"][]).map((controlType: DynamicFormItem["controlType"], i: number) => {
+        return {
+            key: controlType,
+            label: \`\${controlType} label\`,
+            description: \`\${controlType} description\`,
+            multi: false,
+            order: i,
+            options: [
+                ...options.map((e) => {
+                    return { ...e, key: \`\${e.key}-multi-single\` };
+                }),
+            ],
+            controlType,
+            ...COMMON_ITEM,
+        };
+    });
+
+    const multiManyItems: DynamicFormItem[] = (["Dropdown", "Option"] as DynamicFormItem["controlType"][]).map((controlType: DynamicFormItem["controlType"], i: number) => {
+        return {
+            key: controlType,
+            label: \`\${controlType} label\`,
+            description: \`\${controlType} description\`,
+            multi: true,
+            order: i,
+            options: [
+                ...options.map((e) => {
+                    return { ...e, key: \`\${e.key}-multi-many\` };
+                }),
+            ],
+            controlType,
+            ...COMMON_ITEM,
+        };
+    });
+
+    const COMMON_SECTION: Partial<DynamicFormItem> = {
+        wrappingElement: "section",
+        additionalProps: {
+            className: "row d-flex flex-wrap mb-2",
+        },
+    };
+
+    const sections: DynamicFormSection[] = [
+        {
+            key: "value-items-section",
+            title: "Simple single values only",
+            items: valueItems,
+            ...COMMON_SECTION,
+        },
+        {
+            key: "multi-single-items-section",
+            title: "Choose one of many options",
+            items: multiSingleItems,
+            ...COMMON_SECTION,
+        },
+        {
+            key: "multi-many-items-section",
+            title: "Choose any of multiple options",
+            items: multiManyItems,
+            ...COMMON_SECTION,
+        },
+    ];
+
+    const [renderForm, state,, setErrors] = useDynamicForm(sections);
+
+    const validate = () => {
+        setErrors({
+            "value-items-section": {
+                Text: "Text error message",
+                Textarea: "Textarea error message",
+                Checkbox: "Checkbox error message",
+                Datepicker: "Datepicker error message",
+                Stepper: "Stepper error message",
+            },
+            "multi-single-items-section": {
+                Radio: "Radio error message",
+                Dropdown: "Dropdown error message",
+            },
+            "multi-many-items-section": {
+                Dropdown: "Dropdown error message",
+                Option: "Option error message",
+            },
+        });
+    };
+
+    return (
+        <>
+            <div>{renderForm()}</div>
+            <div>
+                <Button onClick={validate}>
+                    Show errors
+                </Button>
+                <Button className="ml-3" onClick={() => alert(JSON.stringify(state, null, 4))}>
+                    Show current values
+                </Button>
+            </div>
+        </>
+    );
+};
 
 export default ShowMeEverything;
                     `}
@@ -521,23 +821,18 @@ const ShowMeEverything: React.FC = () => {
         },
     };
 
-    const valueItems: DynamicFormItem[] = (["Text", "Datepicker", "Checkbox"] as DynamicFormItem["controlType"][]).map((controlType: DynamicFormItem["controlType"], i: number) => {
-        let item: DynamicFormItem = {
-            key: controlType,
-            label: `${controlType} label`,
-            description: `${controlType} description`,
-            order: i,
-            controlType,
-            ...COMMON_ITEM,
-        };
-
-        if (controlType !== "Checkbox") {
-            item.rulerKey = "Checkbox";
-            item.condition = true;
+    const valueItems: DynamicFormItem[] = (["Text", "Textarea", "Datepicker", "Stepper", "Checkbox"] as DynamicFormItem["controlType"][]).map(
+        (controlType: DynamicFormItem["controlType"], i: number) => {
+            return {
+                key: controlType,
+                label: `${controlType} label`,
+                description: `${controlType} description`,
+                order: i,
+                controlType,
+                ...COMMON_ITEM,
+            };
         }
-
-        return { ...item };
-    });
+    );
 
     const options: DynamicFormOption[] = [
         { key: "one", label: "One", value: "1" },
@@ -589,25 +884,42 @@ const ShowMeEverything: React.FC = () => {
     const sections: DynamicFormSection[] = [
         {
             key: "value-items-section",
+            title: "Simple single values only",
             items: valueItems,
+            ...COMMON_SECTION,
+        },
+        {
+            key: "multi-single-items-section",
+            title: "Choose one of many options",
+            items: multiSingleItems,
+            ...COMMON_SECTION,
+        },
+        {
+            key: "multi-many-items-section",
+            title: "Choose any of multiple options",
+            items: multiManyItems,
             ...COMMON_SECTION,
         },
     ];
 
-    const [renderForm, state, , setErrors, meta, touched] = useDynamicForm(sections);
-
-    console.log("touched: ");
-    console.log(touched);
-    console.log("META: ");
-    console.log(JSON.stringify(meta, null, 4));
-    console.log("--------");
+    const [renderForm, state, , setErrors] = useDynamicForm(sections);
 
     const validate = () => {
         setErrors({
             "value-items-section": {
                 Text: "Text error message",
+                Textarea: "Textarea error message",
+                Checkbox: "Checkbox error message",
                 Datepicker: "Datepicker error message",
                 Stepper: "Stepper error message",
+            },
+            "multi-single-items-section": {
+                Radio: "Radio error message",
+                Dropdown: "Dropdown error message",
+            },
+            "multi-many-items-section": {
+                Dropdown: "Dropdown error message",
+                Option: "Option error message",
             },
         });
     };
