eaf206a7b656b3321ea8ee851be2690966ec5c58
Ewe Seong, Yeoh <eweseong.y@gmail.com>
2021-11-25 18:16:56 +0800

    docs(breadcrumb): add label to breadcrumb example home icon

--- docs/src/pages/docs/breadcrumb.tsx
+++ docs/src/pages/docs/breadcrumb.tsx
@@ -4,7 +4,8 @@ import { Breadcrumb, BreadcrumbItem } from "@sebgroup/react-components/Breadcrum
 import { useDynamicForm } from "@sebgroup/react-components/hooks/useDynamicForm";
 
 const homeIcon: JSX.Element = (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1.3em" fill="currentColor" viewBox="0 0 16 16" style={{ verticalAlign: "baseline" }}>
+    <svg xmlns="http://www.w3.org/2000/svg" width="1.3em" fill="currentColor" viewBox="0 0 16 16" style={{ verticalAlign: "baseline" }} aria-labelledby="homeTitle" role="img">
+        <title id="homeTitle">Home</title>
         <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z" />
     </svg>
 );
