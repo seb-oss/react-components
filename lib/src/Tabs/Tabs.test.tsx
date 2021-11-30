9978a1fb1033a6ba14dbd4d07cd113ee73124c45
Kher Phay Chang <kayphay92@gmail.com>
2021-11-30 08:35:06 +0800

    refactor(tabs): add keyboard support

--- lib/src/Tabs/Tabs.test.tsx
+++ lib/src/Tabs/Tabs.test.tsx
@@ -1,10 +1,32 @@
 import React from "react";
 import { unmountComponentAtNode, render } from "react-dom";
-import { Tabs, TabItem } from ".";
+import { Tabs, TabItem, TabsProps } from ".";
 import { act, Simulate } from "react-dom/test-utils";
+
+type TabsKeyboardEventTestCase = {
+    statement: string;
+    triggerEvent: () => void;
+    expectedOutcome: () => void;
+    props: TabsProps;
+};
 
 describe("Component: Tabs", () => {
     let container: HTMLDivElement = null;
+    const tabItems: string[] = ["First", "Second", "Third"];
+
+    const renderComponent = ({ children, ...props }: TabsProps = {}, items: string[] = []) => {
+        act(() => {
+            render(
+                <Tabs {...props}>
+                    {items.map((tab: string) => (
+                        <TabItem key={tab}>{tab}</TabItem>
+                    ))}
+                    {children}
+                </Tabs>,
+                container
+            );
+        });
+    };
 
     beforeEach(() => {
         container = document.createElement("div");
@@ -18,9 +40,7 @@ describe("Component: Tabs", () => {
     });
 
     it("Should render", () => {
-        act(() => {
-            render(<Tabs />, container);
-        });
+        renderComponent();
         expect(container).toBeDefined();
         expect(container.firstElementChild.tagName.toLowerCase()).toEqual("ul");
         expect(container.firstElementChild.classList.contains("rc")).toBeTruthy();
@@ -30,53 +50,76 @@ describe("Component: Tabs", () => {
     });
 
     it("Should render tab items correctly", () => {
-        act(() => {
-            render(
-                <Tabs>
-                    <TabItem>First</TabItem>
-                    <TabItem>Second</TabItem>
-                </Tabs>,
-                container
-            );
-        });
+        renderComponent({}, tabItems);
 
         const items = container.querySelectorAll("a");
 
-        expect(items).toHaveLength(2);
+        expect(items).toHaveLength(tabItems.length);
         expect(items.item(0).getAttribute("data-index-number")).toEqual("0");
         expect(items.item(1).getAttribute("data-index-number")).toEqual("1");
     });
 
     it("Should render non HTML elements", () => {
-        act(() => {
-            render(
-                <Tabs>
-                    <TabItem>First</TabItem>
-                    <TabItem>Second</TabItem>
-                    test123
-                </Tabs>,
-                container
-            );
-        });
-
+        renderComponent({ children: "test123" }, tabItems);
         expect(container.firstElementChild.textContent).toContain("test123");
     });
 
     it("Should fire onTabChange when an item is clicked", () => {
         const onTabChange: jest.Mock = jest.fn();
-
-        act(() => {
-            render(
-                <Tabs value={0} onTabChange={onTabChange}>
-                    <TabItem>First</TabItem>
-                    <TabItem>Second</TabItem>
-                </Tabs>,
-                container
-            );
-        });
+        renderComponent({ value: 0, onTabChange }, tabItems);
 
         act(() => Simulate.click(container.querySelectorAll("a").item(1)));
 
         expect(onTabChange).toBeCalledWith(1);
+    });
+
+    describe("Tabs keyboard event", () => {
+        const onTabChange: jest.Mock = jest.fn();
+        const onTabDelete: jest.Mock = jest.fn();
+        const testCases: Array<TabsKeyboardEventTestCase> = [
+            {
+                statement: "focus next element on arrow right button pressed",
+                triggerEvent: () => Simulate.keyDown(container.querySelector(".nav-tabs"), { key: "ArrowRight" }),
+                expectedOutcome: () => expect(document.activeElement.textContent).toBe(tabItems[2]),
+                props: { value: 1 },
+            },
+            {
+                statement: "focus previous element on arrow left button pressed",
+                triggerEvent: () => Simulate.keyDown(container.querySelector(".nav-tabs"), { key: "ArrowLeft" }),
+                expectedOutcome: () => expect(document.activeElement.textContent).toBe(tabItems[0]),
+                props: { value: 1 },
+            },
+            {
+                statement: "set tab item as active on spacebar pressed",
+                triggerEvent: () => Simulate.keyDown(container.querySelector(".nav-tabs"), { key: " " }),
+                expectedOutcome: () => expect(onTabChange).toBeCalled(),
+                props: { value: 1, onTabChange },
+            },
+            {
+                statement: "set tab item as active on enter pressed",
+                triggerEvent: () => Simulate.keyDown(container.querySelector(".nav-tabs"), { key: "Enter" }),
+                expectedOutcome: () => expect(onTabChange).toBeCalled(),
+                props: { value: 1, onTabChange },
+            },
+            {
+                statement: "fire onTabDelete on delete pressed",
+                triggerEvent: () => Simulate.keyDown(container.querySelector(".nav-tabs"), { key: "Delete" }),
+                expectedOutcome: () => expect(onTabDelete).toBeCalled(),
+                props: { value: 1, onTabDelete },
+            },
+            {
+                statement: "should set focus element as active on arrow right button pressed when autoSelectOnFocus is set",
+                triggerEvent: () => Simulate.keyDown(container.querySelector(".nav-tabs"), { key: "ArrowRight" }),
+                expectedOutcome: () => expect(onTabChange).toBeCalled(),
+                props: { value: 1, onTabChange, autoSelectOnFocus: true },
+            },
+        ];
+        testCases.forEach((testCase: TabsKeyboardEventTestCase) => {
+            it(testCase.statement, () => {
+                renderComponent(testCase.props, tabItems);
+                testCase.triggerEvent();
+                testCase.expectedOutcome();
+            });
+        });
     });
 });
--- lib/src/Tabs/Tabs.tsx
+++ lib/src/Tabs/Tabs.tsx
@@ -1,22 +1,62 @@
 import React from "react";
 import { TabItemProps } from "./TabItem";
 import classnames from "classnames";
+import { useCombinedRefs } from "../hooks/useCombinedRef";
 
 export type TabsProps = JSX.IntrinsicElements["ul"] & {
     /** Index of focsued tab */
     value?: number;
+    /** set focused index to active index on focus */
+    autoSelectOnFocus?: boolean;
     /** Callback on tab item changed */
     onTabChange?: (index: number) => void;
+    /** Callback on tab item when delete key is triggered */
+    onTabDelete?: (index: number) => void;
 };
 /** Tabs organize and allow navigation between groups of content that are related and at the same level of hierarchy. */
-export const Tabs: React.FC<TabsProps> = React.forwardRef(({ value, onTabChange, ...props }: TabsProps, ref: React.ForwardedRef<HTMLUListElement>) => {
+export const Tabs: React.FC<TabsProps> = React.forwardRef(({ value, onTabChange, autoSelectOnFocus, onTabDelete, ...props }: TabsProps, ref: React.ForwardedRef<HTMLUListElement>) => {
+    const tabsRef: React.MutableRefObject<HTMLUListElement> = useCombinedRefs(ref);
+    const [focusedIndex, setFocusedIndex] = React.useState<number>(value);
+
+    const navigateToTab = (target: HTMLAnchorElement, event: React.MouseEvent | React.KeyboardEvent) => {
+        const nextFocusedIndex: number = parseInt(target.dataset.indexNumber);
+        setFocusedIndex(nextFocusedIndex);
+        target.href?.endsWith("#") && event.preventDefault();
+        if (target.dataset.disabled !== "true") {
+            onTabChange && onTabChange(nextFocusedIndex);
+        }
+    };
     const onClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
-        event.currentTarget.href.endsWith("#") && event.preventDefault();
-        onTabChange && onTabChange(parseInt(event.currentTarget.dataset.indexNumber));
+        navigateToTab(event.currentTarget, event);
+    };
+
+    const onKeyDown = (event: React.KeyboardEvent<HTMLUListElement>) => {
+        const items: HTMLAnchorElement[] = Array.from(tabsRef.current.querySelectorAll(".nav-tabs > .rc.nav-item > .nav-link"));
+        switch (event.key) {
+            case "Enter":
+            case " ":
+                navigateToTab(event.target as HTMLAnchorElement, event);
+                break;
+            case "ArrowLeft":
+            case "ArrowRight":
+                const direction: number = event.key === "ArrowLeft" ? -1 : 1;
+                const nextFocusedIndex: number = focusedIndex + direction;
+                if (nextFocusedIndex >= 0 && nextFocusedIndex < items.length) {
+                    setFocusedIndex(nextFocusedIndex);
+                    items[nextFocusedIndex]?.focus();
+                    if (autoSelectOnFocus) {
+                        navigateToTab(items[nextFocusedIndex], event);
+                    }
+                }
+                break;
+            case "Delete":
+                onTabDelete && onTabDelete(focusedIndex);
+                break;
+        }
     };
 
     return (
-        <ul {...props} ref={ref} className={classnames("rc nav nav-tabs", props.className)} role={props.role || "tablist"}>
+        <ul {...props} ref={tabsRef} className={classnames("rc nav nav-tabs", props.className)} role={props.role || "tablist"} onKeyDown={onKeyDown}>
             {React.Children.map(props.children, (Child: React.ReactElement<TabItemProps>, index: number) => {
                 return React.isValidElement<React.FC<TabItemProps>>(Child)
                     ? React.cloneElement<any>(Child, {
