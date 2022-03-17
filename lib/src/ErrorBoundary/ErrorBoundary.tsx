bb7241a62eea8127a9d6203688b6d2a7476ee013
Ewe Seong, Yeoh <eweseong.y@gmail.com>
2022-03-17 17:39:36 +0800

    feat(error-boundary): add error boundary to handle errors

---
+++ lib/src/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+interface ErrorBoundaryProps {
+    children: React.ReactNode;
+    errorView: React.ReactNode;
+}
+
+interface ErrorBoundaryState {
+    hasError: boolean;
+}
+
+export class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+    constructor(props: ErrorBoundaryProps) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError() {
+        return { hasError: true };
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return this.props.errorView;
+        }
+
+        return this.props.children;
+    }
+}
