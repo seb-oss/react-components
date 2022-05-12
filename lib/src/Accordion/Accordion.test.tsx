47ae80ea41b6aff7fb2e2537b4085c79f474859d
Ewe Seong, Yeoh <eweseong.y@gmail.com>
2022-05-12 15:48:15 +0800

    test(components): migrate accordion test cases to react testing libraries

--- lib/src/Accordion/Accordion.test.tsx
+++ lib/src/Accordion/Accordion.test.tsx
@@ -1,29 +1,22 @@
+import { render, RenderResult, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import React from "react";
-import { act, Simulate } from "react-dom/test-utils";
-import { unmountComponentAtNode, render } from "react-dom";
 import { Accordion, AccordionItem } from ".";
 
 describe("Component: Accordion", () => {
-    let container: HTMLDivElement = null;
+    function getAccordionButtons(): Array<HTMLButtonElement> {
+        return screen.getAllByRole<HTMLButtonElement>("button");
+    }
+
+    function getButtonExpansion(button: HTMLButtonElement): boolean {
+        return JSON.parse(button.getAttribute("aria-expanded"));
+    }
 
     /** To disable Collapse setTimeout calls */
-    beforeAll(() => jest.useFakeTimers());
-
-    beforeEach(() => {
-        container = document.createElement("div");
-        document.body.appendChild(container);
-    });
-
-    afterEach(() => {
-        unmountComponentAtNode(container);
-        container.remove();
-        container = null;
-    });
+    // beforeAll(() => jest.useFakeTimers());
 
     it("Should render correctly", () => {
-        act(() => {
-            render(<Accordion />, container);
-        });
+        const { container }: RenderResult = render(<Accordion />);
         expect(container.firstElementChild.classList.contains("rc")).toBeTruthy();
         expect(container.firstElementChild.classList.contains("accordion")).toBeTruthy();
         expect(container.firstElementChild.id).toBeDefined();
@@ -32,188 +25,153 @@ describe("Component: Accordion", () => {
     it("Should render with id, custom class and alternative style", () => {
         const id: string = "myId";
         const className: string = "myClassname";
-        act(() => {
-            render(<Accordion id={id} className={className} alternative />, container);
-        });
+        const { container }: RenderResult = render(<Accordion id={id} className={className} alternative />);
         expect(container.firstElementChild.id).toEqual(id);
         expect(container.firstElementChild.classList.contains("alternative")).toBeTruthy();
         expect(container.firstElementChild.classList.contains(className)).toBeTruthy();
     });
 
-    it("Should toggle on and off an accordion item", () => {
-        act(() => {
-            render(
-                <Accordion>
-                    <AccordionItem header="First" />
-                    <AccordionItem header="Second" />
-                </Accordion>,
-                container
-            );
-        });
-        const firstButton: HTMLButtonElement = container.querySelectorAll<HTMLButtonElement>("button.btn-link").item(0);
-        act(() => {
-            firstButton.click();
-        });
-        expect(JSON.parse(firstButton.getAttribute("aria-expanded"))).toBeTruthy();
-        act(() => {
-            firstButton.click();
-        });
-        expect(JSON.parse(firstButton.getAttribute("aria-expanded"))).toBeFalsy();
+    it("Should toggle on and off an accordion item", async () => {
+        render(
+            <Accordion>
+                <AccordionItem header="First" />
+                <AccordionItem header="Second" />
+            </Accordion>
+        );
+        const firstButton: HTMLButtonElement = getAccordionButtons()[0];
+        await userEvent.click(firstButton);
+        expect(getButtonExpansion(firstButton)).toBeTruthy();
+        await userEvent.click(firstButton);
+        expect(getButtonExpansion(firstButton)).toBeFalsy();
     });
 
     it("Should allow rendering none elements", () => {
         const text: string = "Some text";
-        act(() => {
-            render(<Accordion>{text}</Accordion>, container);
-        });
-        expect(container.firstElementChild.innerHTML).toEqual(text);
+        render(<Accordion>{text}</Accordion>);
+        expect(screen.getByText(text)).toBeInTheDocument();
     });
 
     describe("Should forward the following props to children accordion items", () => {
         const id: string = "myId";
+        let container: HTMLElement;
         let firstButton: HTMLButtonElement;
 
         beforeEach(() => {
-            act(() => {
-                render(
-                    <Accordion id={id} defaultValue={0}>
-                        <AccordionItem header="First" />
-                        <AccordionItem header="Second" />
-                    </Accordion>,
-                    container
-                );
-            });
-            firstButton = container.querySelectorAll<HTMLButtonElement>("button").item(0);
+            const result: RenderResult = render(
+                <Accordion id={id} defaultValue={0}>
+                    <AccordionItem header="First" />
+                    <AccordionItem header="Second" />
+                </Accordion>
+            );
+            container = result.container;
+            firstButton = getAccordionButtons()[0];
         });
 
-        test("paretnId", () => expect(container.querySelectorAll<HTMLDivElement>(".collapse").item(0).dataset.parent).toEqual(`#${id}`));
+        test("parentId", () => expect(container.querySelectorAll<HTMLDivElement>(".collapse").item(0).dataset.parent).toEqual(`#${id}`));
+
         test("value", () => expect(firstButton.dataset.indexNumber).toEqual("0"));
-        test("active", () => expect(firstButton.getAttribute("aria-expanded")).toEqual("true"));
-        test("onToggle", () => {
-            act(() => {
-                firstButton.click();
-            });
-            expect(firstButton.getAttribute("aria-expanded")).toEqual("false");
+
+        test("active", () => expect(getButtonExpansion(firstButton)).toBeTruthy());
+
+        test("onToggle", async () => {
+            await userEvent.click(firstButton);
+            expect(getButtonExpansion(firstButton)).toBeFalsy();
         });
     });
 
     describe("Should set default expanded using default value and default checked", () => {
         test("defaultValue from parent", () => {
-            act(() => {
-                render(
-                    <Accordion defaultValue={0}>
-                        <AccordionItem header="First" />
-                        <AccordionItem header="Second" />
-                    </Accordion>,
-                    container
-                );
-            });
-            expect(container.querySelector<HTMLButtonElement>("button").getAttribute("aria-expanded")).toEqual("true");
+            render(
+                <Accordion defaultValue={0}>
+                    <AccordionItem header="First" />
+                    <AccordionItem header="Second" />
+                </Accordion>
+            );
+            expect(getButtonExpansion(getAccordionButtons()[0])).toBeTruthy();
         });
+
         test("defaultValue from children", () => {
-            act(() => {
-                render(
-                    <Accordion>
-                        <AccordionItem defaultChecked header="First" />
-                        <AccordionItem header="Second" />
-                    </Accordion>,
-                    container
-                );
-            });
-            expect(container.querySelector<HTMLButtonElement>("button").getAttribute("aria-expanded")).toEqual("true");
+            render(
+                <Accordion>
+                    <AccordionItem defaultChecked header="First" />
+                    <AccordionItem header="Second" />
+                </Accordion>
+            );
+            expect(getButtonExpansion(getAccordionButtons()[0])).toBeTruthy();
         });
     });
 
-    it("Should fire on toggle", () => {
+    it("Should fire on toggle", async () => {
         const mockFn: jest.Mock = jest.fn();
-        act(() => {
-            render(
-                <Accordion onToggle={mockFn}>
-                    <AccordionItem header="First" />
-                    <AccordionItem header="Second" />
-                </Accordion>,
-                container
-            );
-        });
-        const firstButton: HTMLButtonElement = container.querySelectorAll<HTMLButtonElement>("button.btn-link").item(0);
-        act(() => {
-            firstButton.click();
-        });
+        render(
+            <Accordion onToggle={mockFn}>
+                <AccordionItem header="First" />
+                <AccordionItem header="Second" />
+            </Accordion>
+        );
+        await userEvent.click(getAccordionButtons()[0]);
         expect(mockFn).toBeCalled();
     });
 
-    it("Should fire accordion item callback on toggle", () => {
+    it("Should fire accordion item callback on toggle", async () => {
         const mockFn: jest.Mock = jest.fn();
-        act(() => {
-            render(
-                <Accordion>
-                    <AccordionItem header="First" onToggle={mockFn} />
-                    <AccordionItem header="Second" />
-                </Accordion>,
-                container
-            );
-        });
-        const firstButton: HTMLButtonElement = container.querySelectorAll<HTMLButtonElement>("button.btn-link").item(0);
-        act(() => {
-            firstButton.click();
-        });
+        render(
+            <Accordion>
+                <AccordionItem header="First" onToggle={mockFn} />
+                <AccordionItem header="Second" />
+            </Accordion>
+        );
+        await userEvent.click(getAccordionButtons()[0]);
         expect(mockFn).toBeCalled();
     });
 
     describe("Keyboard support", () => {
         function renderAccordion(): void {
-            act(() => {
-                render(
-                    <Accordion>
-                        <AccordionItem header="First" />
-                        <AccordionItem header="Second" />
-                    </Accordion>,
-                    container
-                );
-            });
-            container.querySelector<HTMLButtonElement>("button").focus();
-        }
-
-        function pressKey(key: string): void {
-            act(() => Simulate.keyDown(document.activeElement, { key }));
+            render(
+                <Accordion>
+                    <AccordionItem header="First" />
+                    <AccordionItem header="Second" />
+                </Accordion>
+            );
+            getAccordionButtons()[0].focus();
         }
 
         function assertFocusedElement(element: HTMLElement): void {
             expect(document.activeElement).toEqual(element);
         }
 
-        it("Should focus on next header when down arrow button is pressed", () => {
+        it("Should focus on next header when down arrow button is pressed", async () => {
             renderAccordion();
-            assertFocusedElement(container.querySelectorAll<HTMLButtonElement>("button")[0]);
-            pressKey("ArrowDown");
-            assertFocusedElement(container.querySelectorAll<HTMLButtonElement>("button")[1]);
-            pressKey("ArrowDown");
-            assertFocusedElement(container.querySelectorAll<HTMLButtonElement>("button")[0]);
+            assertFocusedElement(getAccordionButtons()[0]);
+            await userEvent.keyboard("[ArrowDown]");
+            assertFocusedElement(getAccordionButtons()[1]);
+            await userEvent.keyboard("[ArrowDown]");
+            assertFocusedElement(getAccordionButtons()[0]);
         });
 
-        it("Should focus on previous header when up arrow button is pressed", () => {
+        it("Should focus on previous header when up arrow button is pressed", async () => {
             renderAccordion();
-            assertFocusedElement(container.querySelectorAll<HTMLButtonElement>("button")[0]);
-            pressKey("ArrowUp");
-            assertFocusedElement(container.querySelectorAll<HTMLButtonElement>("button")[1]);
-            pressKey("ArrowUp");
-            assertFocusedElement(container.querySelectorAll<HTMLButtonElement>("button")[0]);
+            assertFocusedElement(getAccordionButtons()[0]);
+            await userEvent.keyboard("[ArrowUp]");
+            assertFocusedElement(getAccordionButtons()[1]);
+            await userEvent.keyboard("[ArrowUp]");
+            assertFocusedElement(getAccordionButtons()[0]);
         });
 
-        it("Should focus on first header when home button is pressed", () => {
+        it("Should focus on first header when home button is pressed", async () => {
             renderAccordion();
-            assertFocusedElement(container.querySelectorAll<HTMLButtonElement>("button")[0]);
-            pressKey("ArrowDown");
-            assertFocusedElement(container.querySelectorAll<HTMLButtonElement>("button")[1]);
-            pressKey("Home");
-            assertFocusedElement(container.querySelectorAll<HTMLButtonElement>("button")[0]);
+            assertFocusedElement(getAccordionButtons()[0]);
+            await userEvent.keyboard("[ArrowDown]");
+            assertFocusedElement(getAccordionButtons()[1]);
+            await userEvent.keyboard("[Home]");
+            assertFocusedElement(getAccordionButtons()[0]);
         });
 
-        it("Should focus on last header when end button is pressed", () => {
+        it("Should focus on last header when end button is pressed", async () => {
             renderAccordion();
-            assertFocusedElement(container.querySelectorAll<HTMLButtonElement>("button")[0]);
-            pressKey("End");
-            assertFocusedElement(container.querySelectorAll<HTMLButtonElement>("button")[1]);
+            assertFocusedElement(getAccordionButtons()[0]);
+            await userEvent.keyboard("[End]");
+            assertFocusedElement(getAccordionButtons()[1]);
         });
 
         it("Should not handle button event when button is pressed on element aside from accordion header", () => {
