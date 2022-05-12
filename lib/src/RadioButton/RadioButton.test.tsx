df34c2ce41bee347f1c0a118257135c121d42f64
Ewe Seong, Yeoh <eweseong.y@gmail.com>
2022-05-12 18:25:48 +0800

    test(components): migrate button test cases to react testing libraries

--- lib/src/RadioButton/RadioButton.test.tsx
+++ lib/src/RadioButton/RadioButton.test.tsx
@@ -1,61 +1,35 @@
+import { render, RenderResult, screen } from "@testing-library/react";
 import React from "react";
-import { unmountComponentAtNode, render } from "react-dom";
-import { act } from "react-dom/test-utils";
 import { RadioButton } from "./RadioButton";
 
 describe("Component: RadioButton", () => {
-    let container: HTMLDivElement = null;
-
-    beforeEach(() => {
-        container = document.createElement("div");
-        document.body.appendChild(container);
-    });
-
-    afterEach(() => {
-        unmountComponentAtNode(container);
-        container.remove();
-        container = null;
-    });
-
     it("Should render", () => {
-        act(() => {
-            render(<RadioButton />, container);
-        });
-        expect(container).toBeDefined();
+        render(<RadioButton />);
+        expect(screen.getByRole("radio")).toBeInTheDocument();
     });
 
     it("Should pass custom class and id", () => {
         const className: string = "myRadiobuttonClass";
         const wrapperClassname: string = "myWrapperClassname";
-
-        act(() => {
-            render(<RadioButton className={className} wrapperProps={{ className: wrapperClassname }} />, container);
-        });
-        expect(container.querySelector(".custom-control-input").classList.contains(className)).toBeTruthy();
+        const { container }: RenderResult = render(<RadioButton className={className} wrapperProps={{ className: wrapperClassname }} />);
+        expect(screen.getByRole("radio")).toHaveClass(className);
         expect(container.firstElementChild.classList.contains(wrapperClassname)).toBeTruthy();
     });
 
     it("Should render with random id if id is not passed", () => {
-        act(() => {
-            render(<RadioButton>label</RadioButton>, container);
-        });
-        expect(container.querySelector("input").hasAttribute("id")).toBeTruthy();
-        expect(container.querySelector("label").getAttribute("for")).toBe(container.querySelector("input").getAttribute("id"));
+        const { rerender }: RenderResult = render(<RadioButton>label</RadioButton>);
+        expect(screen.getByRole("radio")).toHaveAttribute("id", expect.any(String));
+        expect(screen.getByLabelText("label")).toBeInTheDocument();
 
         const id: string = "myId";
-        act(() => {
-            render(<RadioButton id={id}>label</RadioButton>, container);
-        });
-        expect(container.querySelector("input").id).toEqual(id);
-        expect(container.querySelector("label").getAttribute("for")).toEqual(id);
+        rerender(<RadioButton id={id}>label</RadioButton>);
+        expect(screen.getByRole("radio")).toHaveAttribute("id", id);
+        expect(screen.getByLabelText("label")).toBeInTheDocument();
     });
 
     it("Should render and display label", () => {
         const label: string = "my label";
-        act(() => {
-            render(<RadioButton>{label}</RadioButton>, container);
-        });
-        expect(container.querySelector(`.custom-control-label`)).not.toBeNull();
-        expect(container.querySelector(`.custom-control-label`).innerHTML).toContain(label);
+        render(<RadioButton>{label}</RadioButton>);
+        expect(screen.getByLabelText(label)).toBeInTheDocument();
     });
 });
